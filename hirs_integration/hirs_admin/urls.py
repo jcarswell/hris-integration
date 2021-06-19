10f51df675a850708d7cad2d348c9d8fdb87635d
Josh Carswell <jcarswell@ttdev.ca>
2021-06-19 07:39:47 -0600

    Added names to each view

--- hirs_integration/hirs_admin/urls.py
+++ hirs_integration/hirs_admin/urls.py
@@ -1,19 +1,39 @@
-from hirs_integration.hirs_admin.models import JobRole
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from django.views.generic.edit import FormView
+
 from . import views
 from . import forms
 
 urlpatterns = [
-    path('auth/login/', auth_views.LoginView.as_view(template_name='hirs_admin/login.html')),
-    path('auth/logout/', auth_views.LogoutView.as_view()),
-    path('employee/', views.Employee.as_view()),
-    path('settings/config', views.Settings.as_view()),
-    path('settings/admapping', views.FormView.as_view(form=forms.GroupMapping)),
-    path('settings/patterns', views.FormView.as_view(from=forms.WordList)),
-    path('jobs/', views.FormView.as_view(form=forms.JobRole)),
-    path('designation/', views.FormView.as_view(from=forms.Designation)),
-    path('businessunits/', views.FormView.as_view(from=forms.BusinessUnit)),
-    path('locations/', views.FormView.as_view(form=forms.Location))
+    path('accounts/login/',
+         auth_views.LoginView.as_view(template_name='hirs_admin/login.html'),
+         name='login'),
+    path('accounts/logout/',
+         auth_views.LogoutView.as_view(),
+         name='logout'),
+    path('employee/',
+         views.Employee.as_view(),
+         name='employee'),
+    path('settings/config',
+         views.Settings.as_view(),
+         name='settings'),
+    path('settings/admapping',
+         views.FormView.as_view(form=forms.GroupMapping),
+         name='group_mappings'),
+    path('settings/patterns',
+         views.FormView.as_view(form=forms.WordList),
+         name='patterns'),
+    path('jobs/',
+         views.FormView.as_view(form=forms.JobRole),
+         name='jobs'),
+    path('designation/',
+         views.FormView.as_view(form=forms.Designation),
+         name='designations'),
+    path('businessunits/',
+         views.FormView.as_view(form=forms.BusinessUnit),
+         name='business_units'),
+    path('locations/',
+         views.FormView.as_view(form=forms.Location),
+         name='location'),
+    path('', views.Index.as_view(), name='index')
 ]
