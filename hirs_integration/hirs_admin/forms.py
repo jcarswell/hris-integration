1d3310e237698a2d986e3473706a41539e9308f7
Josh Carswell <jcarswell@ttdev.ca>
2021-06-20 17:52:36 -0600

    Changed the context for form display and set form name and description to the page name and descrip

--- hirs_integration/hirs_admin/forms.py
+++ hirs_integration/hirs_admin/forms.py
@@ -1,11 +1,14 @@
+import logging
+
 from django import forms
 from django.forms.widgets import ChoiceWidget
 from django.utils.safestring import mark_safe
-from django.utils.translation import gettext_lazy as _
-
+from django.utils.translation import gettext_lazy as _t
 
 from . import models
 from .helpers import adtools
+
+logger = logging.getLogger('hirs_admin.forms')
 
 class Form(forms.ModelForm):
     def as_form(self):
@@ -14,7 +17,9 @@ class Form(forms.ModelForm):
         attrs = {"class":"form-control"}
         attrs_disabled = {"class":"form-control","disabled":True}
 
-        for name,field in self.fields.items():
+        logger.warning(f"Building {self.__class__.__name__} as html form")
+
+        for name,_ in self.fields.items():
             bf = self[name]
             if bf.is_hidden:
                 hidden_fields.append(bf.as_hidden)
@@ -32,6 +37,8 @@ class Form(forms.ModelForm):
         if hidden_fields:
             output.append(hidden_fields)
 
+        logger.warning(f"output: {output}")
+
         return mark_safe('\n'.join(output))
 
     def __str__(self) -> str:
@@ -39,7 +46,7 @@ class Form(forms.ModelForm):
 
 
 class GroupMapping(Form):
-    name = _("Group Mappings to Jobs")
+    name = _t("Group Mappings to Jobs")
     class Meta:
         model = models.GroupMapping
         fields = '__all__'
@@ -47,42 +54,42 @@ class GroupMapping(Form):
             'dn': ChoiceWidget(choices=adtools.get_adgroups()),
         }
         labels = {
-            'dn': _("AD Group"),
-            'jobs': _("Jobs"),
-            'bu': _("Business Units"),
-            'loc': _("Locations")
+            'dn': _t("AD Group"),
+            'jobs': _t("Jobs"),
+            'bu': _t("Business Units"),
+            'loc': _t("Locations")
         }
         disabled = ()
         
 
 
 class JobRole(Form):
-    name = _("Job Roles")
+    name = _t("Job Roles")
     class Meta:
         model = models.JobRole
         fields = ('job_id','name','bu','seats')
         labels= {
-            'job_id': _("Job Number"),
-            'name': _("Job Name"),
-            'bu': _("Business Units"),
-            'seats': _("Number of Seats")
+            'job_id': _t("Job Number"),
+            'name': _t("Job Name"),
+            'bu': _t("Business Units"),
+            'seats': _t("Number of Seats")
         }
         disabled = ('job_id',)
 
 
 class Designation(Form):
-    name = _("Employee Designations")
+    name = _t("Employee Designations")
     class Meta:
         model = models.EmployeeDesignation
         fields = ('label',)
         labels = {
-            'label': _("Designation")
+            'label': _t("Designation")
         }
         disabled = ()
         
 
 class BusinessUnit(Form):
-    name = _("Business Unit")
+    name = _t("Business Unit")
 
     class Meta:
         model = models.BusinessUnit
@@ -91,32 +98,32 @@ class BusinessUnit(Form):
             'ad_ou': ChoiceWidget(choices=adtools.get_adous()),
         }
         labels = {
-            'bu_id': _("Number"),
-            'name': _("Name"),
-            'parent': _("Parent"),
-            'ad_ou': _("Active Directory Folder")
+            'bu_id': _t("Number"),
+            'name': _t("Name"),
+            'parent': _t("Parent"),
+            'ad_ou': _t("Active Directory Folder")
         }
         disabled = ('bu_id',)
 
 
 class Location(Form):
-    name = _("Building")
+    name = _t("Building")
     class Meta:
         model = models.Location
         fields = ('bld_id','name')
         labels = {
-            'bld_id': _("Number"),
-            'name': _("Name")
+            'bld_id': _t("Number"),
+            'name': _t("Name")
         }
         disabled = ('bld_id',)
 
 
 class WordList(Form):
-    name = _("Word Expansion Map")
+    name = _t("Word Expansion Map")
     class Meta:
         model = models.WordList
         fields = ('src','replace')
         labels = {
-            'src': _("Source Pattern"),
-            'replace': _("Substitution")
+            'src': _t("Source Pattern"),
+            'replace': _t("Substitution")
         }
