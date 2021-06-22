b21c954320989387cc00ca367fd5747aee193f7d
Josh Carswell <jcarswell@ttdev.ca>
2021-06-22 06:36:55 -0600

    converted to _t for gettext_lazy

--- hirs_integration/hirs_admin/apps.py
+++ hirs_integration/hirs_admin/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _t
 
 class HirsAdminConfig(AppConfig):
     name = 'hirs_admin'
-    verbose_name = _('HIRS Integration Admin')
+    verbose_name = _t('HIRS Integration Admin')
     default_auto_field = 'django.db.models.AutoField'
