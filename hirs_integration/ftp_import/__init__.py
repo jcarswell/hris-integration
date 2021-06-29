a85135fcc345daa0ab5eb800ad874d115b147abd
Josh Carswell <jcarswell@ttdev.ca>
2021-06-29 08:25:19 -0600

    finished the __all__ definition. Added a setup funtion to pre-populate the database

--- hirs_integration/ftp_import/__init__.py
+++ hirs_integration/ftp_import/__init__.py
@@ -1,4 +1,12 @@
-from .exceptions import CSVParsingException
-from .csv import CsvImport
+from .exceptions import CSVParsingException,ConfigurationError,ObjectCreationError
 from .apps import FTPImportConfig
+from .ftp import FTPClient
+from .forms import form
+from .helpers import settings
 
+__all__ = ('FTPClient','FTPImportConfig','FTPClient','form',
+           'CSVParsingException','ConfigurationError','ObjectCreationError',
+           'setup')
+
+def setup():
+    settings.configuration_fixures()
