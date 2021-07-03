d852853fc962a7ae0c02d18fce7d31a742c568a6
Josh Carswell <jcarswell@ttdev.ca>
2021-07-03 16:01:26 -0600

    Setting manager update. Added set_job function for adding or updating jobs. Added get_job function for getting specific job by name. Fixed checking logic

--- hirs_integration/cron/helpers/config.py
+++ hirs_integration/cron/helpers/config.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Set
 
 from hirs_integration.hirs_admin.models import Setting
 
@@ -36,7 +37,7 @@ def configuration_fixures():
             hidden=value[1]
             value = value[0]
 
-        obj,new = Setting.objects.get_or_create(setting=PATH % (catagory,item))
+        obj,new = Setting.o2.get_or_create(setting=PATH % (catagory,item))
         if new:
             obj.setting = PATH % (catagory,item)
             obj.hidden = hidden
@@ -50,8 +51,8 @@ def configuration_fixures():
             for item,data in val.items():
                 add_fixture(key,item,data)
 
-def get_jobs(keep_disabled=False) -> list:
-    jobs = Setting.objects.get_by_path(GROUP_JOBS)
+def get_jobs(keep_disabled=False) -> dict:
+    jobs = Setting.o2.get_by_path(GROUP_JOBS)
     output = {}
     for job in jobs:
         if job.item in ITEM_JOBS:
@@ -63,28 +64,48 @@ def get_jobs(keep_disabled=False) -> list:
 
     for job in output:
         logger.debug(f"Job Config {job} - {jobs[job]}")        
-        if output[job].keys not in [ITEM_PATH,ITEM_SCHEDULE]:
+        if output[job].keys != list(ITEM_JOBS):
             logger.error(f"job {job} is missing required paramaters. Exluding job")
             output.pop(job)
         else:
             output[job][ITEM_SCHEDULE] = CronJob(output[job][ITEM_SCHEDULE])
+            output[job][ITEM_STATE] = bool(output[job][ITEM_STATE])
         if not output[job][ITEM_STATE] and not keep_disabled:
             logger.debug(f"job {job} is disabled")
             output.pop(job)
 
-    return list(output.values())
+    return output
+
+def get_job(job_name:str, create=False) -> dict:
+    jobs = Setting.o2.get_by_path(GROUP_JOBS,job_name)
+    output = {}
+    for job in jobs:
+        if job.item in ITEM_JOBS:
+            logger.warning(f"Got invalid config item {job.item_text} for job {job.catagory_text}")
+        else:
+            output[job.item] = [job.value,job]
+
+    logger.debug(f"Job Config {job_name} - {output}")        
+    if output.keys != list(ITEM_JOBS):
+        logger.error(f"job {job} is missing required paramaters. Exluding job")
+        return {}
+    else:
+        output[ITEM_SCHEDULE][0] = CronJob(output[job][ITEM_SCHEDULE][0])
+        output[ITEM_STATE][0] = bool(output[ITEM_STATE][0])
+
+    return output
 
 def get_config(catagory:str ,item:str) -> str:
     if not catagory in CATAGORY_SETTINGS:
         return ValueError(f"Invalid Catagory requested valid options are: {CATAGORY_SETTINGS}")
     
     try:
-        q = Setting.objects.get_by_path(GROUP_CONFIG,catagory,item)
+        q = Setting.o2.get_by_path(GROUP_CONFIG,catagory,item)
     except Setting.DoesNotExist:
         if item in CONFIG_DEFAULTS[GROUP_CONFIG]:
             configuration_fixures()
             try:
-                q = Setting.objects.get_by_path(GROUP_CONFIG,catagory,item)
+                q = Setting.o2.get_by_path(GROUP_CONFIG,catagory,item)
             except Setting.DoesNotExist:
                 logger.fatal("Failed to install fixture data")
                 raise SystemError(f"Installation of fixture data failed.")
@@ -93,3 +114,19 @@ def get_config(catagory:str ,item:str) -> str:
             raise ValueError(f"Unable to find requested item {item}")
     
     return q.value
+
+def set_job(name, path, schedule, args, state):
+    query_path = GROUP_JOBS + Setting.FIELD_SEP + name + Setting.FIELD_SEP + '%s'
+    def save(setting,value):
+        o,_ = Setting.o2.get_or_create(setting=setting)
+        if o.value != str(value):
+            o.value = str(value)
+            o.save()
+            return True
+        else:
+            return False
+        
+    save(query_path % ITEM_SCHEDULE,schedule)
+    save(query_path % ITEM_PATH, path)
+    save(query_path % ITEM_ARGS, args)
+    save(query_path % ITEM_STATE, state)
