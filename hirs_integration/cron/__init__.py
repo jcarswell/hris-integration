7ebde8cc5535184394cbe2ed6d487f83016f8037
Josh Carswell <jcarswell@ttdev.ca>
2021-07-11 13:10:24 -0600

    Added missing setup method

--- hirs_integration/cron/__init__.py
+++ hirs_integration/cron/__init__.py
@@ -1,4 +1,8 @@
 from .apps import CronConfig
 from .excpetions import AlreadyExists,ModuleOrMethodInvalid
 
-__all__ = ("CronConfig",'AlreadyExists','ModuleOrMethodInvalid')
+__all__ = ("Setup","CronConfig",'AlreadyExists','ModuleOrMethodInvalid')
+
+def setup():
+    from .helpers import config
+    config.configuration_fixures()
