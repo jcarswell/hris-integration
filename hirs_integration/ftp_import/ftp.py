jcarswell/hris-integration

commit 421487d0c4ed6dd4f90332a726a2adaa264b8c3f

config naming cleanup and module standardization

hirs_integration/ftp_import/ftp.py
@@ -6,7 +6,7 @@ import os
 from django import conf
 from tempfile import TemporaryFile
 
-from .helpers import settings
+from .helpers import config
 from .helpers.text_utils import int_or_str
 from .exceptions import ConfigurationError,SFTPIOError
 from .models import FileTrack
@@ -29,13 +29,13 @@ class FTPClient:
             ConfigurationError: configured protocol is not supported.
         """
 
-        self.server = settings.get_config(settings.SERVER_CONFIG,settings.CONF_SERVER)
-        self.port = settings.get_config(settings.SERVER_CONFIG,settings.CONF_PORT)
-        self.user = settings.get_config(settings.SERVER_CONFIG,settings.CONF_USER)
-        basepath = settings.get_config(settings.SERVER_CONFIG,settings.CONF_PATH)
-        file_expr = settings.get_config(settings.SERVER_CONFIG,settings.CONG_FILE_EXP)
-        protocol = settings.get_config(settings.SERVER_CONFIG,settings.CONF_PROTOCAL)
-        self.__password = settings.get_config(settings.SERVER_CONFIG,settings.CONF_PASSWORD)
+        self.server = config.get_config(config.CAT_SERVER,config.SERVER_SERVER)
+        self.port = config.get_config(config.CAT_SERVER,config.SERVER_PORT)
+        self.user = config.get_config(config.CAT_SERVER,config.SERVER_USER)
+        basepath = config.get_config(config.CAT_SERVER,config.SERVER_PATH)
+        file_expr = config.get_config(config.CAT_SERVER,config.SERVER_FILE_EXP)
+        protocol = config.get_config(config.CAT_SERVER,config.SERVER_PROTOCAL)
+        self.__password = config.get_config(config.CAT_SERVER,config.SERVER_PASSWORD)
         
         self.file_expr = re.compile(file_expr)
         self.basepath = ''
@@ -73,7 +73,7 @@ class FTPClient:
         """
 
         logger.info(f"Connecting to {self.server}")
-        paramiko.util.log_to_file(str(conf.settings.LOG_DIR) + '\\ftp_client.log')
+        paramiko.util.log_to_file(str(conf.config.LOG_DIR) + '\\ftp_client.log')
         
         self.sock = paramiko.Transport((self.server, self.port))
         try:

hirs_integration/ftp_import/__init__.py
@@ -4,9 +4,10 @@ from .apps import FTPImportConfig
 __all__ = ('setup','run','FTPImportConfig','CSVParsingException','ConfigurationError','ObjectCreationError')
 
 def setup():
-    from .helpers import settings
+    from .helpers import config
     from cron.helpers.job_manager import Job
-    settings.configuration_fixures()
+
+    config.configuration_fixures()
     cj = Job('ftp_import')
     cj.new('ftp_import.run','0 */6 * * *')
 

hirs_integration/ftp_import/csv.py
@@ -4,7 +4,7 @@ import importlib
 
 from string import ascii_letters,digits
 
-from .helpers import settings
+from .helpers import config
 from .exceptions import ConfigurationError, ObjectCreationError
 
 logger = logging.getLogger('ftp_import.CSVImport')
@@ -27,8 +27,8 @@ class CsvImport():
         
         self.fields = []
         self.data = []
-        self.sep = settings.get_config(settings.CSV_CONFIG,settings.CSV_FIELD_SEP)
-        self.form = settings.get_config(settings.CSV_CONFIG,settings.CSV_IMPORT_CLASS)
+        self.sep = config.get_config(config.CAT_CSV,config.CSV_FIELD_SEP)
+        self.form = config.get_config(config.CAT_CSV,config.CSV_IMPORT_CLASS)
 
         self.parse_headers(file_handle)
         self.parse_data(file_handle)
@@ -36,7 +36,7 @@ class CsvImport():
         #TODO: if self.import_errors -> send notification email
 
     def parse_headers(self, file_handle) -> None:
-        import_fields = settings.get_fields()
+        import_fields = config.get_fields()
         
         # ensure we're at the start of the file to grab the header row
         file_handle.seek(0)
@@ -70,7 +70,7 @@ class CsvImport():
         if len(new_fields) > 0:
             #Add the new fields to the configuration and re re-run parse_headers 
             #TODO: Added some smart logic to try and parse feilds automagically
-            conf = settings.CsvSetting()
+            conf = config.CsvSetting()
             conf.add(*new_fields)
             self.import_fields = conf.fields
             self.fields = []

hirs_integration/ftp_import/forms.py
@@ -8,7 +8,7 @@ from hirs_admin import forms
 from django.utils.datastructures import MultiValueDict
 from distutils.util import strtobool
 
-from .helpers import settings
+from .helpers import config
 from .exceptions import ObjectCreationError
 from .helpers.text_utils import int_or_str
 
@@ -41,7 +41,7 @@ class EmployeeForm():
         self.address = forms.EmployeeAddress
         self.kwargs = kwargs
         self._feild_config = fields_config
-        self.expand = strtobool(settings.get_config(settings.CSV_CONFIG,settings.CSV_USE_EXP))
+        self.expand = strtobool(config.get_config(config.CAT_CSV,config.CSV_USE_EXP))
         
         #FIXME: Should get fields from from note model
         fields = get_fields(Employee,EmployeePhone,EmployeeAddress,exclude="employee")
@@ -122,7 +122,7 @@ class EmployeeForm():
 
     def _location_check(self,data):
         loc,new = Location.objects.get_or_create(pk=data)
-        loc_desc = settings.get_config(settings.FIELD_CONFIG,settings.FIELD_LOC_NAME)
+        loc_desc = config.get_config(config.CAT_FIELD,config.FIELD_LOC_NAME)
 
         if new and loc_desc not in self.kwargs:
             logger.error(f"Location description field, {loc_desc} not in fields imported")
@@ -148,8 +148,8 @@ class EmployeeForm():
         """
         logger.debug(f"Checking for job role with id {data}")
         job,new = JobRole.objects.get_or_create(pk=data)
-        job_desc = settings.get_config(settings.FIELD_CONFIG,settings.FIELD_JD_NAME)
-        bu_id = settings.get_config(settings.FIELD_CONFIG,settings.FIELD_JD_BU)
+        job_desc = config.get_config(config.CAT_FIELD,config.FIELD_JD_NAME)
+        bu_id = config.get_config(config.CAT_FIELD,config.FIELD_JD_BU)
         if new and job_desc not in self.kwargs:
             logger.error(f"Job description field, {job_desc} not in fields imported")
             raise ObjectCreationError(f"Job description field, {job_desc} not in fields")
@@ -197,8 +197,8 @@ class EmployeeForm():
         """
         logger.debug(f"Checking for business unit with id {data} ")
         bu,new = BusinessUnit.objects.get_or_create(pk=data)
-        bu_desc = settings.get_config(settings.FIELD_CONFIG,settings.FIELD_BU_NAME)
-        bu_parent_field = settings.get_config(settings.FIELD_CONFIG,settings.FIELD_BU_PARENT)
+        bu_desc = config.get_config(config.CAT_FIELD,config.FIELD_BU_NAME)
+        bu_parent_field = config.get_config(config.CAT_FIELD,config.FIELD_BU_PARENT)
         if new and bu_desc not in self.kwargs:
             logger.error(f"Business unit name field, {bu_desc} not in fields imported")
             raise ObjectCreationError(f"Job description field, {bu_desc} not in fields")

hirs_integration/ftp_import/helpers/config.py
@@ -4,24 +4,23 @@ from distutils.util import strtobool
 from warnings import warn
 from hirs_admin.models import Setting
 
-__all__ = ('CsvSetting','get_fields','get_config','SERVER_CONFIG','CSV_CONFIG','FIELD_CONFIG')
-
 logger = logging.getLogger("ftp_import.helpers")
 
 #CONSTANTS
-SERVER_CONFIG = 'server'
-CSV_CONFIG = 'csv_parse'
-FIELD_CONFIG = 'field_config'
-SETTINGS_GROUP = 'ftp_import_config'
-SETTINGS_CATAGORIES = (SERVER_CONFIG,CSV_CONFIG,FIELD_CONFIG)
-CONF_SERVER = 'server'
-CONF_PROTOCAL = 'protocal'
-CONF_PORT = 'port'
-CONF_USER = 'user'
-CONF_PASSWORD = 'password'
-CONF_SSH_KEY = 'ssh_key'
-CONF_PATH = 'base_path'
-CONG_FILE_EXP = 'file_name_expression'
+GROUP_CONFIG = 'ftp_import_config'
+GROUP_MAP = 'ftp_import_feild_mapping'
+CAT_SERVER = 'server'
+CAT_CSV = 'csv_parse'
+CAT_FIELD = 'field_config'
+SETTINGS_CATAGORIES = (CAT_SERVER,CAT_CSV,CAT_FIELD)
+SERVER_SERVER = 'server'
+SERVER_PROTOCAL = 'protocal'
+SERVER_PORT = 'port'
+SERVER_USER = 'user'
+SERVER_PASSWORD = 'password'
+SERVER_SSH_KEY = 'ssh_key'
+SERVER_PATH = 'base_path'
+SERVER_FILE_EXP = 'file_name_expression'
 CSV_FIELD_SEP = 'field_sperator'
 CSV_TXT_QULIFY = 'csv_text_qualifier'
 CSV_FAIL_NOTIF = 'import_failure_notification_email'
@@ -32,31 +31,32 @@ FIELD_JD_NAME = 'job_description_name_field'
 FIELD_JD_BU = 'job_description_business_unit_field'
 FIELD_BU_NAME = 'business_unit_name_field'
 FIELD_BU_PARENT = 'business_unit_parent_field'
-MAP_GROUP = 'ftp_import_feild_mapping'
 FIELD_ITEMS = ('import','map_to')
-DEFAULTS = {
+
+IMPORT_DEFAULTS = {
     'import': 'False',
     'map_to': ''
 }
+
 CONFIG_DEFAULTS = {
-    SERVER_CONFIG: {
-        CONF_SERVER: None,
-        CONF_PROTOCAL: 'sftp',
-        CONF_PORT: '22',
-        CONF_USER: None,
-        CONF_PASSWORD: [None,True],
-        CONF_SSH_KEY: [None,True],
-        CONF_PATH: '.',
-        CONG_FILE_EXP: '.*'
+    CAT_SERVER: {
+        SERVER_SERVER: None,
+        SERVER_PROTOCAL: 'sftp',
+        SERVER_PORT: '22',
+        SERVER_USER: None,
+        SERVER_PASSWORD: [None,True],
+        SERVER_SSH_KEY: [None,True],
+        SERVER_PATH: '.',
+        SERVER_FILE_EXP: '.*'
     },
-    CSV_CONFIG: {
+    CAT_CSV: {
         CSV_FIELD_SEP: ',',
         CSV_TXT_QULIFY: '',
         CSV_FAIL_NOTIF: '',
         CSV_IMPORT_CLASS: 'ftp_import.forms',
         CSV_USE_EXP: 'True'
     },
-    FIELD_CONFIG: {
+    CAT_FIELD: {
         FIELD_LOC_NAME: None,
         FIELD_JD_NAME: None,
         FIELD_JD_BU: None,
@@ -67,7 +67,7 @@ CONFIG_DEFAULTS = {
 
 
 class CsvSetting():
-    PATH_FORMAT = MAP_GROUP + Setting.FIELD_SEP + '%s' + Setting.FIELD_SEP + '%s'
+    PATH_FORMAT = GROUP_MAP + Setting.FIELD_SEP + '%s' + Setting.FIELD_SEP + '%s'
 
     def __init__(self) -> None:
         self.fields = {}
@@ -75,7 +75,7 @@ class CsvSetting():
 
     def get(self) -> None:
         fields = {}
-        for row in Setting.o2.get_by_path(MAP_GROUP):
+        for row in Setting.o2.get_by_path(GROUP_MAP):
             if row.catagory not in fields:
                 fields[row.catagory] = {
                     "import": 'False',
@@ -108,7 +108,7 @@ class CsvSetting():
                 for item in FIELD_ITEMS:
                     s = Setting()
                     s.setting = self.PATH_FORMAT % (arg,item)
-                    s.value = DEFAULTS[item]
+                    s.value = IMPORT_DEFAULTS[item]
                     logger.info(f"Adding new field: {s}")
                     s.save()
 
@@ -145,7 +145,7 @@ class CsvSetting():
 
 def configuration_fixures():
     def add_fixture(catagory,item,value):
-        PATH = SETTINGS_GROUP + Setting.FIELD_SEP + '%s' + Setting.FIELD_SEP + '%s'
+        PATH = GROUP_CONFIG + Setting.FIELD_SEP + '%s' + Setting.FIELD_SEP + '%s'
 
         hidden = False
         
@@ -178,15 +178,15 @@ def get_config(catagory:str ,item:str) -> str:
     if not catagory in SETTINGS_CATAGORIES:
         return ValueError(f"Invalid Catagory requested valid options are: {SETTINGS_CATAGORIES}")
     
-    q = Setting.o2.get_by_path(SETTINGS_GROUP,catagory,item)
+    q = Setting.o2.get_by_path(GROUP_CONFIG,catagory,item)
     if len(q) == 0 and item in CONFIG_DEFAULTS[catagory]:
         configuration_fixures()
-        q = Setting.o2.get_by_path(SETTINGS_GROUP,catagory,item)
+        q = Setting.o2.get_by_path(GROUP_CONFIG,catagory,item)
         if len(q) == 0:
             logger.fatal("Failed to install fixture data")
             raise SystemError(f"Installation of fixture data failed.")
     elif len(q) == 0:
-        logger.error(f"Setting {SETTINGS_GROUP}/{catagory}/{item} was requested but does not exist")
+        logger.error(f"Setting {GROUP_CONFIG}/{catagory}/{item} was requested but does not exist")
         raise ValueError(f"Unable to find requested item {item}")
 
     return q[0].value

commit 3e5ce6532d7c1771860ffd80030a1fa8bc9742c0

fix bug introduced in 421487d0c4ed6dd4f90332a726a2adaa264b8c3f

hirs_integration/ftp_import/ftp.py
@@ -73,7 +73,7 @@ class FTPClient:
         """
 
         logger.info(f"Connecting to {self.server}")
-        paramiko.util.log_to_file(str(conf.config.LOG_DIR) + '\\ftp_client.log')
+        paramiko.util.log_to_file(str(conf.settings.LOG_DIR) + '\\ftp_client.log')
         
         self.sock = paramiko.Transport((self.server, self.port))
         try: