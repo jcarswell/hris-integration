b8da10499194628c107c2bc8bd0c23ab68ae3144
Josh Carswell <jcarswell@ttdev.ca>
2021-07-24 07:59:57 -0600

    Added Exception to init

--- hirs_integration/smtp_client/__init__.py
+++ hirs_integration/smtp_client/__init__.py
@@ -1,6 +1,7 @@
 from .apps import SmtpClientConfig
+from .exceptions import SmtpServerError,SmtpToInvalid
 
-__all__ = ('SmtpClientConfig','setup')
+__all__ = ('SmtpClientConfig','setup','SmtpServerError','SmtpToInvalid')
 
 def setup():
     from .helpers import config
