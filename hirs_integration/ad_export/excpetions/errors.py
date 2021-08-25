d6b88e5cc4c752e2fe0863741b6a8e685709d079
Josh Carswell <jcarswell@ttdev.ca>
2021-08-25 10:49:57 -0600

    Added ConfigError and ADCreateError. ADResultError catures row_count

--- hirs_integration/ad_export/excpetions/errors.py
+++ hirs_integration/ad_export/excpetions/errors.py
@@ -4,8 +4,21 @@ class BaseException(Exception):
 
 class ADResultsError(BaseException):
     """The specified job already exists"""
-    pass
+    def __init__(self, *args, row_count:int =None) -> None:
+        self.row_count = row_count
+        super().__init__(*args)
+
 
 class UserDoesNotExist(BaseException):
     """The Request user doesn't exist"""
+    pass
+
+
+class ConfigError(BaseException):
+    """Raised when there is an issue with the configuration"""
+    pass
+
+
+class ADCreateError(BaseException):
+    """Raised when creating and AD Object Fails"""
     pass
