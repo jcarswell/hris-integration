84eb91b260f62d39c225070ef60c9d8d209e7735
Josh Carswell <jcarswell@ttdev.ca>
2021-09-04 10:54:56 -0600

    Added get_id function to get the ID from the location

--- hirs_integration/hirs_admin/static/hirs_admin/dist/js/base.js
+++ hirs_integration/hirs_admin/static/hirs_admin/dist/js/base.js
@@ -9,18 +9,27 @@ function isInt(value) {
 
 function get_basepath() {
   var loc = location.pathname.split('/');
-  loc.pop()
+  loc.pop();
   if (isInt(loc[loc.length-1])) {
     loc.pop();
   }
   return loc.join('/') + '/';
 }
 
+function get_id() {
+  var loc = location.pathname.split('/');
+  loc.pop();
+  if (isInt(loc[loc.length-1])) {
+    return parseInt(loc[loc.length-1]);
+  }
+  return undefined;
+}
+
 function set_nav() {
   var loc = get_basepath();
   $('nav li a').each(function() {
-    $this = $(this)[0]
-    ref = $this.getAttribute('href')
+    $this = $(this)[0];
+    ref = $this.getAttribute('href');
     if (ref !== null && ref.indexOf(loc) !== -1) {
       $this.setAttribute('class',$this.className + ' active');
     }
