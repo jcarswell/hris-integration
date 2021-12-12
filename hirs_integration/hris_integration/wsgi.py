71e66b60e275cb4a9c447f1f1c1cc84fe442d595
Josh Carswell <Josh.Carswell@thecarswells.ca>
2021-12-12 15:54:45 -0600

    Renamed core project code #15

--- hirs_integration/hris_integration/wsgi.py
+++ hirs_integration/hris_integration/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hirs_integration.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hris_integration.settings')
 
 application = get_wsgi_application()
