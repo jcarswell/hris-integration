7e34f0d942ddf002dbeb67fa95e3ee97d74b5619
Josh Carswell <Josh.Carswell@thecarswells.ca>
2021-12-11 18:36:03 -0600

    Added import_field_map_to choices based on avalible field in the Employee Model

--- hirs_integration/ftp_import/validators.py
+++ hirs_integration/ftp_import/validators.py
@@ -1,8 +1,9 @@
 import importlib
 
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _t
 
-__all__ = ('import_validator','import_field_choices')
+__all__ = ('import_validator','import_field_choices','import_field_map_to')
 
 def import_validator(value) -> None:
     try:
@@ -21,3 +22,23 @@ def import_field_choices():
     fields = get_fields()
     for field in fields.keys():
         yield((field,field))
+
+import_field_map_to = [
+    ('emp_id', _t("Employee ID")),
+    ('start_date', _t("Start Date")),
+    ('manager',_t("Manager")),
+    ('givename',_t("Given Name")),
+    ('middlename',_t("Middle Name")),
+    ('surname',_t("Surname")),
+    ('suffix',_t("Suffix")),
+    ('state',_t("State")),
+    ('leave',_t("Leave State")),
+    ('type',_t("Employee Type")),
+    ('username',_t("Username")),
+    ('primary_job',_t("Primary Job")),
+    ('secondary_jobs',_t("Secondary Job(s)")),
+    ('location',_t("Location")),
+    ('email_alias',_t("Email Alias")),
+    ('password',_t("Password"))    
+]
+    
