56a80745fe29b69470df4cbb1e9bb41d206b6938
Josh Carswell <Josh.Carswell@thecarswells.ca>
2021-12-11 21:12:47 -0600

    Added module specific validators and choices module

---
+++ hirs_integration/corepoint_export/validators.py
@@ -0,0 +1,26 @@
+from common.validators import import_validator,ValidationError
+from django.utils.translation import gettext_lazy as _t
+
+__all__= ('employee_fields')
+
+employee_fields = [
+    ('phone',_t('Phone Number')),
+    ('designations',_t('Designations')),
+    ('address',_t('Address')),
+    ('firstname',_t('First Name')),
+    ('lastname',_t('Last Name')),
+    ('username',_t('Username')),
+    ('location',_t('Location')),
+    ('status',_t('Status')),
+    ('id',_t('Employee ID')),
+    ('photo',_t('Photo')),
+    ('bu',_t('Business Unit Name')),
+    ('manager',_t('Manager')),
+    ('upn',_t('User Principal Name')),
+    ('bu_id',_t('Businss Unit ID')),
+    ('email',_t('Email Address')),
+    ('is_supervisor',_t('Is Supervisor')),
+    ('employeetype',_t('Employee Type')),
+    ('title',_t('Title')),
+    ('manager_id',_t('Manager ID'))
+]
