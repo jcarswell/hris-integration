e75f7724c76e109e252a629b214a472e7450b255
Josh Carswell <Josh.Carswell@thecarswells.ca>
2021-12-11 20:32:52 -0600

    moved import_validator to common and imported it here

--- hirs_integration/ftp_import/validators.py
+++ hirs_integration/ftp_import/validators.py
@@ -1,21 +1,7 @@
-import importlib
-
-from django.core.exceptions import ValidationError
+from common.validators import import_validator,ValidationError
 from django.utils.translation import gettext_lazy as _t
 
-__all__ = ('import_validator','import_field_choices','import_field_map_to')
-
-def import_validator(value) -> None:
-    try:
-        lib = importlib.import_module(value)
-        if not hasattr(lib,'form'):
-            raise ValueError
-    except ModuleNotFoundError:
-        raise ValidationError(f"{value} does not exist",
-                              params={'value': value})
-    except ValueError:
-        raise ValidationError(f"{value} is missing the 'form' method",
-                              params={'value': value})
+__all__ = ('import_field_choices','import_field_map_to')
         
 def import_field_choices():
     from ftp_import.helpers.config import get_fields
