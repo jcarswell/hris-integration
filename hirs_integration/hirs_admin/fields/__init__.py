5b7f28726411f5bd13641cfd7bc5ccd409fb0abb
Josh Carswell <Josh.Carswell@thecarswells.ca>
2021-12-17 07:08:50 -0600

    Setting field generator now returns the field and value manager for each setting

--- hirs_integration/hirs_admin/fields/__init__.py
+++ hirs_integration/hirs_admin/fields/__init__.py
@@ -1,56 +1,97 @@
 import logging
-import json
+import re
 
-from django.forms.fields import Field
+from django.forms.fields import (CharField,RegexField,BooleanField,ChoiceField,
+                                 IntegerField,FloatField,DecimalField,DateField,
+                                 DateTimeField)
 from distutils.util import strtobool
+from common.functions import FieldConversion
 
 from hirs_admin.models import Setting
+from hirs_admin import validators,widgets
 
-def SettingFieldGenerator(setting:Setting) -> Field:
+logger = logging.getLogger("hirs_admin.fields")
+
+def SettingFieldGenerator(setting:Setting):
     def clean_kwargs(kwargs:dict, extra:list =None) -> dict:
         base = ['required','widget','label','initial','help_text','error_messages','show_hidden_initial',
-                'localize','disabled','label_suffix']
+                'localize','disabled','label_suffix','validators']
         if extra:
             base = base + extra
 
+        keys={}
         for k in kwargs.keys():
-            if k not in base:
-                kwargs.pop(k)
-        
-        return kwargs
+            if k in base:
+                keys[k] = kwargs[k]
+        keys.setdefault('label',setting.item_text)
+        if 'validators' in kwargs.keys():
+            keys['validators'] = []
+            for validator in kwargs['validators']:
+                try:
+                    keys['validators'].append(eval(validator))
+                except NameError:
+                    logger.warn(f"validator \"{validator}\" is not valid")
+
+        return keys
 
     if not isinstance(setting,Setting):
-        raise ValueError(f"Expexted Settings object got {type(setting)}")
-
-    setting_data = json.loads(setting.field_properties)
-    setting_data['label'] = setting.item_text
+        raise TypeError(f"Expexted Settings object got {setting.__class__.__name__}")
 
     try:
-        field = eval(setting_data['type'])
-        setting_data.pop('type')
+        field = eval(setting.field_properties['type'])
     except NameError:
-        raise ValueError(f"settings data is not valid, missing field_type")
+        raise ValueError(f"settings data is not valid, missing field type")
+    value = FieldConversion(setting.field_properties['type'])
+    value(setting.value)
 
-    if setting_data['type'] in ('CharField','RegexField'):
-        args = ['max_length','min_length','strip','empty_value']
-        setting_data.setdefault('max_length',256)
-        
-        return field(setting.value,**clean_kwargs(setting_data,args))
+    if setting.field_properties['type'] == 'CharField':
+        kw = clean_kwargs(setting.field_properties,['max_length','min_length','strip','empty_value'])
+        kw.setdefault('max_length',768)
+        kw.setdefault('label',setting.item_text)
+        if setting.hidden:
+            kw.setdefault('widget',widgets.Hidden)
+        return field(**kw),value
 
-    elif setting_data['type'] == 'BooleanField':
-        field(strtobool(setting.value,**clean_kwargs(setting_data)))
+    elif setting.field_properties['type'] == 'RegexField':
+        kw = clean_kwargs(setting.field_properties,['max_length','min_length','strip','empty_value'])
+        kw.setdefault('max_length',768)
+        kw.setdefault('label',setting.item_text)
+        if setting.hidden:
+            kw.setdefault('widget',widgets.Hidden)
+        return field(value,**kw),value
 
-    elif setting_data['type'] in ('ChoiceField','MultipleChoiceField'):
-        args = ['Choices']
-        return field(setting.value,**clean_kwargs(setting_data,['choices']))
+    elif setting.field_properties['type'] == 'BooleanField':
+        kw = clean_kwargs(setting.field_properties)
+        return field(**kw),value
 
-    elif setting_data['type'] in ('IntegerField','FloatField'):
-        args = ['max_value','min_value']
-        return field(setting.value,**clean_kwargs(setting_data,args))
+    elif setting.field_properties['type'] == 'ChoiceField':
+        kw = clean_kwargs(setting.field_properties,['choices'])
+        if 'choices' in kw:
+            if isinstance(kw['choices'],str):
+                try:
+                    kw['choices'] = eval(kw['choices'])
+                except NameError:
+                    logger.warn(f"{setting.item} does not seem to have valid choice selection")
+        return field(**kw),value
 
-    elif setting_data['type'] == 'DecimalField':
-        args = ['max_value','min_value','max_digits','decimal_places']
-        return field(setting.value,**clean_kwargs(setting_data,args))
+    elif setting.field_properties['type'] == 'MultipleChoiceField':
+        kw = clean_kwargs(setting.field_properties,['choices'])
+        if 'choices' in kw:
+            if isinstance(kw['choices'],str):
+                try:
+                    kw['choices'] = eval(kw['choices'])
+                except NameError:
+                    logger.warn(f"{setting.item} does not seem to have valid choice selection")
+        return field(**kw),value
 
-    elif setting_data['type'] in ('DateTimeField','DateField'):
-        return field(setting.value,**clean_kwargs(setting_data,['input_formats']))
+    elif setting.field_properties['type'] in ('IntegerField','FloatField'):
+        kw = clean_kwargs(setting.field_properties,['max_value','min_value'])
+        return field(**kw),value
+
+    elif setting.field_properties['type'] == 'DecimalField':
+        kw = clean_kwargs(setting.field_properties,['max_value','min_value','max_digits','decimal_places'])
+        return field(**kw),value
+
+    elif setting.field_properties['type'] in ('DateTimeField','DateField'):
+        kw = clean_kwargs(setting.field_properties,['input_formats'])
+        return field(**kw),value
