7462a72e86f2b3a1ca819280c5b2f031687a3d2c
Josh Carswell <Josh.Carswell@thecarswells.ca>
2021-12-18 12:49:35 -0600

    install_service is now a local command instead of living in setup

--- hirs_integration/cron/__init__.py
+++ hirs_integration/cron/__init__.py
@@ -1,8 +1,27 @@
+import os
+
+from django.conf import settings
+
 from .apps import CronConfig
 from .exceptions import AlreadyExists,ModuleOrMethodInvalid
 
-__all__ = ("Setup","CronConfig",'AlreadyExists','ModuleOrMethodInvalid')
+__all__ = ("Setup","install_service","CronConfig",'AlreadyExists','ModuleOrMethodInvalid')
 
 def setup():
     """No setup tasks required at this time"""
     pass
+
+def install_service():
+    import subprocess
+    import sys
+
+    nssm = str(settings.BASE_DIR) + "\\bin\\nssm.exe"
+    srv_name = 'hirs_integration_cron'
+    if not os.path.exists(nssm):
+        print(f"Sorry the nssm executable doesn't seem to exist at {nssm}")
+        sys.exit(-1)
+
+    subprocess.run([nssm,'install',srv_name,sys.executable])
+    subprocess.run([nssm,'set',srv_name,'AppParameters','%s\\cron\\service.py'])
+    subprocess.run([nssm,'set',srv_name,'AppDirectory',str(settings.BASE_DIR)])
+    subprocess.run([nssm,'set',srv_name,'AppStdout',str(settings.LOG_DIR) + "\\cron_service.out"])
