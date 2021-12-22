158e93736c5d3d7eb2eb00193ec9d7f7ad489485
Josh Carswell <Josh.Carswell@thecarswells.ca>
2021-12-22 10:39:30 -0600

    Actual validator for cron scheduls which CronJob wasn't doing

--- hirs_integration/cron/validators.py
+++ hirs_integration/cron/validators.py
@@ -1,10 +1,51 @@
 from django.core.exceptions import ValidationError
+from string import digits
 
 __all__ = ('cron_validator',)
 
 def cron_validator(value):
-    try:
-        from cron.helpers.data_structures import CronJob
-        CronJob(value)
-    except ValueError:
-        ValidationError("string is not a valid cron string")
+    def char_validate(str_list):
+        for c in str_list:
+            if c not in ['*','/',','] + list(digits):
+                raise ValidationError("got invlaid charater in cron job. Allowed characters are 0-9, * and /")
+
+    def place_validate(place,min,max):
+        if '/' in list(place):
+            values = place.split('/')
+            if values[0] != '*':
+                raise ValidationError(f"cron value with step must start with *: {place}")
+            if not min <= int(values[1]) <= max:
+                raise ValidationError(f"cron step value '{values[1]}'' outside of valid range '{min}-{max}'")
+        elif ',' in list(place):
+            values = place.split(',')
+            for v in values:
+                place_validate(v,min,max)
+        elif place != '*' and not min <= int(place) <= max:
+            raise ValidationError(f"cron value '{place}'' outside of valid range '{min}-{max}'")
+
+    if not isinstance(value,str):
+        raise ValidationError(f"Expected str got {value.__class__.__name__}")
+    
+    cron = value.split()
+    if len(cron) != 5:
+        raise ValidationError(f"Cron schedule should be 6 segments. got {len(cron)}")
+    
+    # cron[0] == minute
+    char_validate(list(cron[0]))
+    place_validate(cron[0],0,59)
+    
+    # cron[1] == hour
+    char_validate(list(cron[1]))
+    place_validate(cron[1],0,23)
+
+    # cron[2] == day
+    char_validate(list(cron[2]))
+    place_validate(cron[2],1,31)
+
+    # cron[3] == month
+    char_validate(list(cron[3]))
+    place_validate(cron[3],1,12)
+
+    # cron[4] == day of week
+    char_validate(list(cron[4]))
+    place_validate(cron[4],0,6)
