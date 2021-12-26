e58f6bc30b711bf75fb145920ab7a24fd670ab05
Josh Carswell <jcarswell@ttdev.ca>
2021-12-26 11:33:09 -0600

    Fixeder up. done and function process no work. Added alert-container populate

--- hirs_integration/hirs_admin/static/hirs_admin/dist/js/alerts.js
+++ hirs_integration/hirs_admin/static/hirs_admin/dist/js/alerts.js
@@ -1,19 +1,35 @@
-function doneProcess(ret_data) {
+function doneProcess(ret_data,focus) {
+  $('.is-invalid',focus).removeClass("is-invalid");
+  $('.alert').alert('close');
+  createAlert();
   if (ret_data["status"] != "success") {
-    $('.alert').text(ret_data["message"]);
-    $('.alert').alert();
+    $('button:submit',focus).removeClass('btn-primary').addClass('btn-danger');
+    $('#alert-inner').html(ret_data['errors'].join('<br>'));
     $('.alert').addClass("alert-danger");
-    ret_data["fields"].forEach(i => {$("input[name="+i+"]").addClass('is-invalid');})
-
+    ret_data.fields.forEach(e => {$('input[name="'+e+'"]').addClass('is-invalid');})
+    $('#alert-container').removeClass('d-none')
   } else {
-    $('.alert').text("Success");
-    $('.alert').alert();
-    $('.alert').addClass("alert-sucess");
-    $('.is-invalid').removeClass("is-invalid");
+    $('button:submit',focus).removeClass('btn-primary').removeClass('btn-danger').addClass('btn-success');
+    $('#alert-container').addClass("d-none");
   }
 }
-function errorProcess(jqXHR,status,error) {
-  $('.alert').text("A Server error occured: " + status);
-  $('.alert').alert();
-  $('.alert').addClass("alert-danger");
+function errorProcess(jqXHR,status,error,focus) {
+  $('button:submit',focus).removeClass('btn-primary').addClass('btn-danger');
+  $('#alert-container').removeClass('d-none');
+  $(".alert").addClass('alert-danger');
+  $("#alert-inner").html("<storng>"+status+":</strong> "+error);
 }
+function createAlert() {
+  var alertBody = `  <div class="alert" role="alert">
+  <div id="alert-inner"></div>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>`
+  $('#alert-container').html(alertBody);
+}
+
+$('.alert').on('closed.bs.alert',function() {
+  $('#alert-container').addClass('d-none');
+  createAlert();
+})
