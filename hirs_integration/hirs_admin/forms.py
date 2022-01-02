1d6cf3d5cf348f513cb73e04638249f38af8aa79
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-01-02 10:33:42 -0600

    Added Manual Import Form

--- hirs_integration/hirs_admin/forms.py
+++ hirs_integration/hirs_admin/forms.py
@@ -1,11 +1,13 @@
 import logging
 
 from django import forms
-from django.forms.widgets import CheckboxInput, Select
+from django.db.models import manager
+from django.forms.widgets import Select
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _t
+from common.functions import model_to_choices
 
-from . import models
+from . import models,widgets
 from .helpers import adtools
 
 logger = logging.getLogger('hirs_admin.forms')
@@ -205,3 +207,98 @@ class EmployeePending(Form):
         self.fields['manager'].widget.attrs.update({'data-live-search': 'true','data-style':'bg-white'})
         self.fields['state'].widget.attrs.update({'data-style':'bg-white'})
         self.fields['leave'].widget.attrs.update({'data-style':'bg-white'})
+
+
+class ManualImportForm(forms.Form):
+    emp_id = forms.IntegerField(label="Employee ID",
+                                min_value=1,required=True)
+    givenname = forms.CharField(label="Given Name",
+                                max_length=96,required=True,
+                                help_text="Provide actual Given name, first name defined on pending object will be preserved")
+    middlename = forms.CharField(label="Middle Name",
+                                 max_length=96,required=False)
+    surname = forms.CharField(label="Surname",
+                              max_length=96,required=True,
+                              help_text="Provide actual surname, last name defined on pending object will be preserved")
+    state = forms.BooleanField(label="State",
+                               widget=widgets.CheckboxInput(attrs={"class":"form-control"}),
+                               initial=True)
+    leave = forms.BooleanField(label="On Leave",
+                               widget=widgets.CheckboxInput(attrs={"class":"form-control"}),
+                               initial=False)
+    primary_job = forms.ChoiceField(label="Primary Job",
+                                    widget=widgets.SelectPicker(attrs={"class":"form-control"}),
+                                    required=True,
+                                    choices=model_to_choices(models.JobRole.objects.all()))
+    jobs = forms.MultipleChoiceField(label="Additional Jobs",
+                                     required=False,
+                                     choices=model_to_choices(models.JobRole.objects.all()),
+                                     help_text="Leave blank to use jobs set on pending employee object.")
+    location = forms.ChoiceField(label="Location",
+                                 widget=widgets.SelectPicker(attrs={"class":"form-control"}),
+                                 required=True,
+                                 choices=model_to_choices(models.Location.objects.all()))
+    manager = forms.ChoiceField(label="Manager",
+                                 widget=widgets.SelectPicker(attrs={"class":"form-control"}),
+                                 required=False,
+                                 choices=model_to_choices(models.Employee.objects.all(),True))
+    start_data = forms.DateField(label="Start Date",
+                                 required=True)
+    type = forms.CharField(label="Employee Type",
+                           max_length=64,required=False)
+    field_order = ['emp_id','givenname','middlename','surname',
+                   'primary_job','jobs','manager','location',
+                   'manager','type','start_data']
+
+    def as_form(self):
+        output = []
+        hidden_fields = []
+
+        for name,_ in self.fields.items():
+            classes = ["form-control"]
+            bf = self[name]
+            if bf.is_hidden:
+                hidden_fields.append(bf.as_hidden)
+            else:
+                if bf.label:
+                    label = bf.label_tag() or ''
+                output.append('<div>')
+                output.append(label)
+                output.append(f'<span class="text-secondary">{bf.help_text}</span>')
+                output.append(bf.as_widget(attrs={'class':" ".join(classes)}))
+                output.append('</div>')
+
+        if hidden_fields:
+            output.append(hidden_fields)
+
+        return mark_safe('\n'.join(output))
+
+    def __str__(self) -> str:
+        return self.as_form()
+
+    def save(self,pending_employee:models.EmployeePending):
+        if not self.has_changes():
+            return True
+
+        employee = models.Employee()
+        for field in self.data.keys():
+            if hasattr(employee,field):
+                setattr(employee,field,self.data[field] or self.initial[field])
+
+        employee.guid = pending_employee.guid
+        employee.username = pending_employee.username
+        employee.email_alias = pending_employee.email_alias
+        employee.password = pending_employee.password
+        if self.data["jobs"] == []:
+            for j in pending_employee.jobs:
+                employee.jobs.add(j)
+        
+        employee.save()
+        employee_overrides = models.EmployeeOverrides(employee=employee)
+        if pending_employee.firstname != self.data['givenname']:
+            employee_overrides.firstname = pending_employee.firstname
+        if pending_employee.lastname != self.data['surname']:
+            employee_overrides.lastname = pending_employee.lastname
+        if pending_employee.designation != None:
+            employee_overrides.designations = pending_employee.designation
+            
