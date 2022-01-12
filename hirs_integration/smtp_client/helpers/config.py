158fbad4337efe73a92f18d97b2088f340f4bd0d
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-01-12 07:47:20 -0600

    Added git_catagory method to Config Manager and removed non-standard get_config_cat function

--- hirs_integration/smtp_client/helpers/config.py
+++ hirs_integration/smtp_client/helpers/config.py
@@ -14,22 +14,17 @@ class Config(ConfigurationManagerBase):
     fixtures = CONFIG_DEFAULTS
     Setting = Setting
 
+    def get_catagory(self,catagoty:str) -> dict:
+        output = {}
+        if catagoty not in self.fixtures:
+            raise ValueError(f"Requested Catagory \"{catagoty}\" is not valid")
+        for item in self.fixtures[catagoty]:
+            self.get(catagoty,item)
+            output[item] = self.value
+
+        return output
+
+
 def get_config(catagory:str ,item:str) -> Any:
     """Now depricated use Config instead to manage the value"""
     return Config()(catagory,item)
-
-def get_config_cat(catagory:str) -> dict:
-    if not catagory in CATAGORY_SETTINGS:
-        return ValueError(f"Invalid Catagory requested valid options are: {CATAGORY_SETTINGS}")
-
-    q = Setting.o2.get_by_path(GROUP_CONFIG,catagory)
-
-    if len(q) == 0:
-        logger.error(f"Setting catagory {GROUP_CONFIG}/{catagory} was requested but nothing returned")
-        raise ValueError(f"No items found are you sure the setup has been run")
-
-    output = {}
-    for item in q:
-        output[item.item] = item.value
-
-    return output
