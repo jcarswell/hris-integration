9ce8e44ff064dfdf3c487b1350d84c197bc577d6
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-02-15 06:57:40 -0600

    Merged all form function and initializers to a common file. Addded a form serializer method to get disabled and unchecked fields

--- hirs_integration/hirs_admin/static/hirs_admin/dist/js/form.js
+++ hirs_integration/hirs_admin/static/hirs_admin/dist/js/form.js
@@ -34,8 +34,48 @@ function createAlert() {
 </div>`
   $('#alert-container').html(alertBody);
 }
-
+function is_new() {
+  if (isNaN(get_id())) {return undefined;}
+  else if (get_id() > 0) {return false;}
+  else {return true;}
+}
+function do_delete() {
+  $.ajax({
+    method: "delete",
+    url: location.href,
+    })
+  .done(function(data){
+    location.replace(get_basepath());
+  })
+  .fail(function(jqXHR,Status,error) {errorProcess(jqXHR,status,error);});
+}
+function serialize_form(form,data) {
+  // Ensure that both disable rows and un-checked checkboxes are prased correctly
+  $("#form_list input:disabled",form).each(function(i,e) {
+    data.push({name:e.name,value:e.value});
+  })
+  $("input[type=checkbox]:not(:checked)",this).each(function(i,e) {
+    data.push({name:e.name,value:"off"});
+  })
+  return data
+}
 $('.alert').on('closed.bs.alert',function() {
   $('#alert-container').addClass('d-none');
   createAlert();
 })
+var id = $('#form_list input[type=number]')[0]
+$(function() {
+  if (is_new()) {
+    if (id !== undefined) {
+      id.disabled = false;
+      id.readOnly = false;
+      //$('<input type="submit" name="submit-new" class="btn btn-primary" style="margin: 4pt 0 0 auto;" value="Submit & New"/>').insertBefore($('input[name=submit]'));
+      //$('button[name=submit]').attr('style','margin: 4pt 0 0 4pt;');
+    }
+  }
+  const csrftoken = getCookie('csrftoken')
+  $.ajaxSetup({
+    headers: { "X-CSRFToken": csrftoken }
+  });
+  $('#btn_back')[0].href = get_basepath()
+});
