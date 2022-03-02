jcarswell/hris-integration

commit 1c60b076cad5513c4622103116eca3ddfa3c9296

Ensure that datetime fields are tz aware, #48

hirs_integration/common/functions.py
@@ -4,6 +4,7 @@ import logging
 from typing import Any
 from django.core.exceptions import ValidationError
 from django.utils.dateparse import parse_date,parse_datetime
+from django.utils.timezone import make_aware
 from distutils.util import strtobool
 from django.db.utils import ProgrammingError
 from warnings import warn
@@ -180,7 +181,7 @@ class FieldConversion:
         self.value = parse_date(value)
 
     def DateTimeField(self,value):
-        self.value = parse_datetime(value)
+        self.value = make_aware(parse_datetime(value))
 
     def Pattern(self):
         return str(self.value.pattern)

commit 5cc455b17cf34f014f7a8fac41f4fcedc4936923

#48 correcting bug introduced in 1c60b076

hirs_integration/common/functions.py
@@ -181,7 +181,12 @@ class FieldConversion:
         self.value = parse_date(value)
 
     def DateTimeField(self,value):
-        self.value = make_aware(parse_datetime(value))
+        self.value = parse_datetime(value)
+        try:
+            self.value = make_aware(self.value)
+        except ValueError:
+            # self.value is already tz enabled
+            pass
 
     def Pattern(self):
         return str(self.value.pattern)