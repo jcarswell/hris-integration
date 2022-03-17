beb138d0231389c4a29d550f301b2a7229c3a095
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-03-17 07:50:03 -0600

    Added model fetch to retrieve template instead of looking from subject and body to be passed in.

--- hirs_integration/smtp_client/actions/template.py
+++ hirs_integration/smtp_client/actions/template.py
@@ -1,7 +1,49 @@
 import logging
 
-from hris_integration.data_structures import EmployeeMangaer
-from jinja2 import Environment,PackageLoader
+from jinja2 import Environment,select_autoescape
 
 from smtp_client.smtp import Smtp
-from smtp_client.exceptions import SmtpServerError,SmtpToInvalid
+from smtp_client.exceptions import InvlaidTemplate
+from smtp_client.models import EmailTemplates
+
+logger = logging.getLogger('smtp_client.SmtpTemplate')
+class SmtpTemplate:
+    def __init__(self,template:str =None,subject:str =None,body:str =None,to:str =None,**kwargs):
+        if to is None:
+            raise ValueError("the 'to' argument is required")
+
+        if template:
+            try:
+                template_obj:EmailTemplates = EmailTemplates.objects.get(name=template)
+                subject = template_obj.email_subject
+                body = template_obj.email_body
+            except EmailTemplates.DoesNotExist as e:
+                logger.critical(f"The template '{template}' is no longer valid, please check the configuration")
+                raise InvlaidTemplate(e,template_name=template)
+
+        if subject is None and body is None:
+            raise ValueError("If the template name is not provided then the subject and body fields are required")
+
+        self.env_subject = Environment(
+            autoescape=select_autoescape()
+        )
+        self.env_subject.from_string(subject)
+        self.env_body = Environment(
+            autoescape=select_autoescape()
+        )
+        self.env_subject.from_string(body)
+        self.to = to
+        self.kwargs = kwargs
+
+    def make_context(self,**kwargs) -> dict:
+        context = self.kwargs
+        context.update(kwargs)
+        return context
+
+    def send(self,**kwargs) -> None:
+        context = self.make_context(kwargs)
+        s = Smtp()
+        msg = s.mime_build(html=self.env_body.render(context),
+                           subject=self.env_subject.render(context),
+                           to=self.to)
+        s.send_html(self.to,msg)
