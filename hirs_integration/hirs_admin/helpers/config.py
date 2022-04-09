4554ebe41e318e9ae4ad31ea5ce0691ed6dc2919
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-04-09 09:53:17 -0600

    removed unused configurations

--- hirs_integration/hirs_admin/helpers/config.py
+++ hirs_integration/hirs_admin/helpers/config.py
@@ -2,25 +2,11 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
 
 import logging
-from re import split
-
-from typing import Any
-from common.functions import ConfigurationManagerBase
 
 from hirs_admin.models import Setting
-from .settings_fields import *
 
 logger = logging.getLogger('hirs_admin.config')
 
-class Config(ConfigurationManagerBase):
-    root_group = GROUP_CONFIG
-    catagory_list = CATAGORY_SETTINGS
-    fixtures = CONFIG_DEFAULTS
-    Setting = Setting
-
-def get_config(catagory:str ,item:str) -> Any:
-    """Now deprecated use Config instead to manage the value"""
-    return Config()(catagory,item)
 
 def setting_parse(setting:Setting =None,html_id:str =None):
     """Converts either a Setting object to an html safe id that can be back parsed
