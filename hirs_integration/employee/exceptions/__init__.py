0e6915290cb9edbdea2cab60214825afe2285a92
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-04-10 10:00:28 -0600

    Added licenses and module imports

---
+++ hirs_integration/employee/exceptions/__init__.py
@@ -0,0 +1,6 @@
+# Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+
+from .errors import ProcessError, EmployeeNotFound
+
+__all__ = ('ProcessError', 'EmployeeNotFound')
