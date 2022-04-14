72cae816245b364650c6b3b173a62516f485732e
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-04-14 11:15:47 -0600

    Moved GroupManager to the supporting Organization Model

--- hirs_integration/organization/helpers/group_manager.py
+++ hirs_integration/organization/helpers/group_manager.py
@@ -4,8 +4,8 @@
 import logging
 
 from pyad import ADGroup
-from hirs_admin.models import GroupMapping
-from hirs_admin import validators
+from organization.models import GroupMapping
+from common import validators
 
 logger = logging.getLogger('ad_export.GroupManager')
 
