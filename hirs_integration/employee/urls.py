93b8ae2280a32a7b5498e3016908b71ec9616100
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-04-20 07:11:48 -0600

    Added the import views

--- hirs_integration/employee/urls.py
+++ hirs_integration/employee/urls.py
@@ -2,7 +2,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
 
 from django.urls import path
-from hris_integration.views import ListView
+from hris_integration.views import ListView, FormView
 
 from . import views
 from . import forms
@@ -14,12 +14,12 @@ urlpatterns = [
     path('employee/<int:id>/',
          views.Employee.as_view(),
          name='employee_edit'),
-    path('pending/',
-         ListView.as_view(form=forms.EmployeePending),
-         name='employee_pending'),
-    path('pending/<int:id>/',
-         views.PendingEmployeeEdit.as_view(),
-         name='employee_pending_edit'),
+    path('imported/',
+         ListView.as_view(form=forms.ImportedEmployeeView),
+         name='employee_imported_list'),
+    path('imported/',
+         FormView.as_view(form=forms.ImportedEmployeeView),
+         name='employee_imported_edit'),
     path('manual/<int:id>/',
          views.ManualImport.as_view(),
          name='employee_manual')
