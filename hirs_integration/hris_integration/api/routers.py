51eabc1f14d91c0fbe2e9ee05e8bf54eb095a560
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-04-24 09:47:57 -0600

    added licenses info and __init__

--- hirs_integration/hris_integration/api/routers.py
+++ hirs_integration/hris_integration/api/routers.py
@@ -1,13 +1,13 @@
-from sys import prefix
+# Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+
 from rest_framework import routers
 
-
 class S2Router(routers.DefaultRouter):
-    """A simplified router for select2 ajax calls.
-    """
+    """A simplified router for select2 ajax calls."""
 
     prefix = '_s2'
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for key in self.routes[0].mapping.keys():
--- hirs_integration/hris_integration/api/viewsets.py
+++ hirs_integration/hris_integration/api/viewsets.py
@@ -1,6 +1,8 @@
+# Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated,DjangoModelPermissions,BasePermission
-
 
 class NoAccess(BasePermission):
     """Deny all access to the view."""
@@ -8,12 +10,11 @@ class NoAccess(BasePermission):
         return False
 
 class Select2ViewSet(ModelViewSet):
-    """A limited viewset that provides get actions supporting the Select2 library.
-    """
+    """A limited viewset that provides get actions supporting the Select2 library."""
 
     def get_permissions(self):
-        """Return the permissions for the viewset.
-        """
+        """Return the permissions for the viewset."""
+
         if self.action == 'list':
             return [DjangoModelPermissions]
         else:
