439f21dee235e0f0a2173b9942c5a92dde391a29
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-04-24 09:44:06 -0600

    Addded license information

--- hirs_integration/organization/api/views.py
+++ hirs_integration/organization/api/views.py
@@ -1,3 +1,6 @@
+# Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+
 from hris_integration.api.viewsets import Select2ViewSet
 from rest_framework.viewsets import ModelViewSet
 
