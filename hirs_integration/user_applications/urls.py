1b579893d312e7adcc73d92eaf0332241bc60ee2
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-04-30 07:56:22 -0600

    Added form views

---
+++ hirs_integration/user_applications/urls.py
@@ -0,0 +1,22 @@
+# Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+
+from django.urls import path
+from hris_integration.views import FormView,ListView
+
+from . import forms
+
+urlpatterns = [
+    path('software/<int:id>/',
+         FormView.as_view(form=forms.Software),
+         name='software_edit'),
+    path('software/',
+         ListView.as_view(form=forms.Software),
+         name='software'),
+    path('accounts/<int:id>/',
+         FormView.as_view(form=forms.EmployeeTrackedAccount),
+         name='accounts_edit'),
+    path('accounts/',
+         ListView.as_view(form=forms.EmployeeTrackedAccount),
+         name='accounts'),
+]
