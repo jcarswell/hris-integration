b30e8968ce8d2414d632d6a8fd58c9dac273f3b0
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-04-30 07:09:24 -0600

    Added missing add_software call and modal show

--- hirs_integration/hirs_admin/static/hirs_admin/dist/js/employee.js
+++ hirs_integration/hirs_admin/static/hirs_admin/dist/js/employee.js
@@ -330,6 +330,40 @@ function edit_software(id) {
     $('.modal-header').after(softwareTemplate(software[id]));
     $('.modal-title').text('Edit Software');
     $('.modal-body').after(modalActionsSoftware);
+    $("#modal-default").modal('show');
+}
+function add_software() {
+    $('.modal-header').after(softwareNewTemplate);
+    $('.modal-title').text('Add Software');
+    $('.modal-body').after(modalActionsSoftware);
+    $('#software-form-0 input[type=select]').select2({
+      placeholder: 'Select a software',
+      ajax: {
+        url: select_software_url,
+        dataType: 'json',
+        delay: 250,
+        data: function (params) {
+          return {
+            q: params.term, // search term
+            page: params.page
+          };
+        },
+        processResults: function (data, params) {
+          params.page = params.page || 1;
+          return {
+            results: data.results,
+            pagination: {
+            more: (params.page * 30) < data.total_count
+            }
+          };
+        },
+        cache: true
+      },
+      escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
+      minimumInputLength: 1,
+      theme: 'bootstrap4'
+    });
+    $("#modal-default").modal('show');
 }
 function update_software(e) {
     var data = serialize_json($(e).closest('.modal-content').find('form')[0]);
