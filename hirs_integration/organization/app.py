079d2833673c71737112f726f3f3d8d4217bd3ca
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-04-30 08:53:08 -0600

    Fixed App class name

--- hirs_integration/organization/app.py
+++ hirs_integration/organization/app.py
@@ -4,7 +4,7 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _t
 
-class OrganizationExportConfig(AppConfig):
+class OrganizationConfig(AppConfig):
     name = 'organization'
     verbose_name = _t('Organizational Data App')
     default_auto_field = 'django.db.models.AutoField'
