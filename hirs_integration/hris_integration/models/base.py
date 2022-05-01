840970c2c926b401e5a1b7f04fc5cff38c0ba437
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-05-01 09:03:55 -0600

    Added license and doc strings

--- hirs_integration/hris_integration/models/base.py
+++ hirs_integration/hris_integration/models/base.py
@@ -1,11 +1,17 @@
+# Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+
 from django.db import models
 
-
-class ChangeLogMixin(models.Models):
-
+class ChangeLogMixin(models.Model):
+    """
+    Generic Change Log Mixin that adds the created and updated on date fields.
+    This will also add a change log to the change tracking table."""
     class Meta:
         abstract = True
 
+    #: datetime: The date and time the record was last updated.
     updated_on = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    #: datetime: The date and time the record was created.
     created_on = models.DateTimeField(auto_now=True, null=True, blank=True)
     
