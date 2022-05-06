a2b0c586dc9fb2257c1f9dcd834e58ba63666f68
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-05-06 07:13:55 -0600

    fixed the category typo, added warning to get_config

--- hirs_integration/smtp_client/helpers/config.py
+++ hirs_integration/smtp_client/helpers/config.py
@@ -6,6 +6,7 @@ import logging
 from typing import Any
 from hirs_admin.models import Setting
 from common.functions import ConfigurationManagerBase
+from warnings import warn
 
 from .settings_fields import *
 
@@ -13,21 +14,22 @@ logger = logging.getLogger('SMTP.config')
 
 class Config(ConfigurationManagerBase):
     root_group = GROUP_CONFIG
-    catagory_list = CATAGORY_SETTINGS
+    category_list = CATEGORY_SETTINGS
     fixtures = CONFIG_DEFAULTS
     Setting = Setting
 
-    def get_catagory(self,catagoty:str) -> dict:
+    def get_category(self,category:str) -> dict:
         output = {}
-        if catagoty not in self.fixtures:
-            raise ValueError(f"Requested Catagory \"{catagoty}\" is not valid")
-        for item in self.fixtures[catagoty]:
-            self.get(catagoty,item)
+        if category not in self.fixtures:
+            raise ValueError(f"Requested category \"{category}\" is not valid")
+        for item in self.fixtures[category]:
+            self.get(category,item)
             output[item] = self.value
 
         return output
 
 
-def get_config(catagory:str ,item:str) -> Any:
+def get_config(category:str ,item:str) -> Any:
     """Now deprecated use Config instead to manage the value"""
-    return Config()(catagory,item)
+    warn("get_config is deprecated use Config instead to manage the value", DeprecationWarning)
+    return Config()(category,item)
