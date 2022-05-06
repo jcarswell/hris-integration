d5f3917b25c1e6e1d620a30253e6f022c8e74415
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-05-06 07:34:42 -0600

    moved all settings names migration here as this will be the migration that all further migrations will build from

--- hirs_integration/hirs_admin/migrations/0031_updated_csv_settings.py
+++ hirs_integration/hirs_admin/migrations/0031_updated_csv_settings.py
@@ -5,16 +5,41 @@ from common.functions import configuration_fixtures
 
 logger = logging.getLogger('hirs_admin.Migrations')
 
-
+SETTING_MIGRATIONS = {
+    "global_settings/configuration/ad_search_base_dn": "active_directory/configuration/ad_search_base_dn",
+    "ad_export/employee_configurations/leave_groups_remove": "organizational_configuration/groups/leave_groups",
+    "ad_export/employee_configurations/leave_groups_add": None,
+    "ad_export/configuration/last_sycronization_run": "ad_export/configuration/last_synchronization_run",
+    "ftp_import_feild_mapping": "ftp_import_field_mapping",
+    "ftp_import_config/server/protocal": "ftp_import_config/server/protocol",
+    "ftp_import_config/server/field_sperator": "ftp_import_config/server/field_separator",
+    "ftp_import_config/export_options/actve_status_field_value":"ftp_import_config/export_options/active_status_field_value",
+    "ad_export/user_defaults/orginization": "ad_export/user_defaults/organization",
+    "corepoint_export/configuration/last_sycronization_run": "corepoint_export/configuration/last_synchronization_run",
+    
+    }
 
 def forward_func(apps, schema_editor):
-    from hirs_admin.models import Setting
+    Setting = apps.get_model('hirs_admin', 'Setting')
     # Update FTP Import Module
     try:
         from ftp_import.helpers import settings_fields as ftp_fields
         configuration_fixtures(ftp_fields.GROUP_CONFIG,ftp_fields.CONFIG_DEFAULTS,Setting)
     except ModuleNotFoundError:
         logger.warn("Failed to updated ftp_import setting, app not installed")
+
+    for old_path, new_path in SETTING_MIGRATIONS.items():
+        try:
+            if len(new_path.split('/')) == 1:
+                for setting in Setting.o2.get_by_path(old_path):
+                    setting.setting = "/".join([new_path, setting.setting.split('/')[-1]])
+                    setting.save()
+            setting = Setting.o2.get(setting=old_path)
+            setting.setting = new_path
+            setting.save()
+        except Setting.DoesNotExist:
+            logger.warn(f"Failed to update setting {old_path}")
+
 
 def reverse_func(apps, schema_editor):
     pass
