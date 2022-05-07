d03885cff95bf08ccbc9df25dc287fe5fe342dd3
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-05-07 08:00:51 -0600

    removed exceptions from module

--- hirs_integration/hirs_admin/__init__.py
+++ hirs_integration/hirs_admin/__init__.py
@@ -2,9 +2,8 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
 
 from .apps import HrisAdminConfig
-from .exceptions import RenderError
 
-__all__ = ('HrisAdminConfig','setup','RenderError')
+__all__ = ('HrisAdminConfig','setup')
 
 def setup():
     """No setup tasks required for this module"""
