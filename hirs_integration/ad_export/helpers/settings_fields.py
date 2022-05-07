ff4f45adc77e1f34d8df88a7b8ebe7afbda798c9
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-05-07 08:39:14 -0600

    removed un-needed import

--- hirs_integration/ad_export/helpers/settings_fields.py
+++ hirs_integration/ad_export/helpers/settings_fields.py
@@ -1,8 +1,6 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
 
-from django.utils.translation import gettext_lazy as _t
-
 ## Config Groups
 GROUP_CONFIG = 'ad_export'
 
