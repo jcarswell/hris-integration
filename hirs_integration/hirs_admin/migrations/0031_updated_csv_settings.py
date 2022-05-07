8cf67189d129865fef49dd1ced2e3ce5996ac161
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-05-07 10:45:46 -0600

    Removed config_fixtures call and revised the updated fields to function as expected

--- hirs_integration/hirs_admin/migrations/0031_updated_csv_settings.py
+++ hirs_integration/hirs_admin/migrations/0031_updated_csv_settings.py
@@ -1,7 +1,6 @@
 import logging
 
 from django.db import migrations
-from settings.config_manager import configuration_fixtures
 
 logger = logging.getLogger('hirs_admin.Migrations')
 
@@ -21,24 +20,25 @@ SETTING_MIGRATIONS = {
 
 def forward_func(apps, schema_editor):
     Setting = apps.get_model('hirs_admin', 'Setting')
-    # Update FTP Import Module
-    try:
-        from ftp_import.helpers import settings_fields as ftp_fields
-        configuration_fixtures(ftp_fields.GROUP_CONFIG,ftp_fields.CONFIG_DEFAULTS,Setting)
-    except ModuleNotFoundError:
-        logger.warn("Failed to updated ftp_import setting, app not installed")
 
     for old_path, new_path in SETTING_MIGRATIONS.items():
-        try:
-            if len(new_path.split('/')) == 1:
-                for setting in Setting.o2.get_by_path(old_path):
-                    setting.setting = "/".join([new_path, setting.setting.split('/')[-1]])
-                    setting.save()
-            setting = Setting.o2.get(setting=old_path)
-            setting.setting = new_path
-            setting.save()
-        except Setting.DoesNotExist:
-            logger.debug(f"Failed to update setting {old_path}")
+        if new_path is None:
+            try:
+                setting = Setting.objects.get(setting=old_path)
+                setting.delete()
+            except Setting.DoesNotExist:
+                pass
+        else:
+            try:
+                if len(new_path.split('/')) == 1:
+                    for setting in Setting.objects.filter(setting__startswith=old_path):
+                        setting.setting = "/".join([new_path, setting.setting.split('/')[-1]])
+                        setting.save()
+                setting = Setting.objects.get(setting=old_path)
+                setting.setting = new_path
+                setting.save()
+            except Setting.DoesNotExist:
+                logger.debug(f"Failed to update setting {old_path}")
 
 
 def reverse_func(apps, schema_editor):
