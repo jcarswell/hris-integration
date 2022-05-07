c985d9c1362e610e92a789d16dc843e3e511ad52
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-05-07 08:03:00 -0600

    removed unneeded imports after seperating settings

--- hirs_integration/hirs_admin/views.py
+++ hirs_integration/hirs_admin/views.py
@@ -1,14 +1,8 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
 
-import logging
-
 from django.views.generic.base import TemplateResponseMixin
 from hris_integration.views import LoggedInView
-
-from .helpers import settings_view,config
-
-logger = logging.getLogger('hirs_admin.view')
 
 class Index(TemplateResponseMixin, LoggedInView):
     """Home Page"""
