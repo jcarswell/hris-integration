72f1626b9281969eaeea6d1eb4560c7838f500d2
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-05-07 09:38:30 -0600

    Removed config fixtures give code refactoring

--- hirs_integration/hirs_admin/migrations/0028_migrate_field_properties.py
+++ hirs_integration/hirs_admin/migrations/0028_migrate_field_properties.py
@@ -1,14 +1,12 @@
 import logging
 
 from django.db import migrations, models
-from common.functions import configuration_fixtures
+from settings.config_manager import configuration_fixtures
 
 logger = logging.getLogger('hirs_admin.Migrations')
 
-
-
 def forward_func(apps, schema_editor):
-    from hirs_admin.models import Setting
+    Setting = apps.get_model('hirs_admin', 'Setting')
 
     def update_csv_fields():
         fields = Setting.o2.get_by_path(ftp_fields.GROUP_MAP)
@@ -42,10 +40,6 @@ def forward_func(apps, schema_editor):
                 logger.debug(f"un-matched setting {field} encountered while updating cron_job fields")
             field.save()
 
-    # Update HRIS Admin Module
-    from hirs_admin.helpers import settings_fields as hris_fields
-    configuration_fixtures(hris_fields.GROUP_CONFIG,hris_fields.CONFIG_DEFAULTS,Setting)
-    
     # Update FTP Import Module
     try:
         from ftp_import.helpers import settings_fields as ftp_fields
@@ -54,13 +48,6 @@ def forward_func(apps, schema_editor):
     except ModuleNotFoundError:
         logger.warn("Failed to updated ftp_import setting, app not installed")
     
-    # Update AD Export Module
-    try:
-        from ad_export.helpers import settings_fields as ad_fields
-        configuration_fixtures(ad_fields.GROUP_CONFIG,ad_fields.CONFIG_DEFAULTS,Setting)
-    except ModuleNotFoundError:
-        logger.warn("Failed to updated ad_export settings, app not installed")
-
     # Update cron module
     try:
         from cron.helpers import settings_fields as cron_fields
@@ -69,19 +56,6 @@ def forward_func(apps, schema_editor):
     except ModuleNotFoundError:
         logger.warn("Failed to updated cron settings, app not installed")
 
-    # Update SMTP Client Module
-    try:
-        from smtp_client.helpers import settings_fields as smtp_fields
-        configuration_fixtures(smtp_fields.GROUP_CONFIG,smtp_fields.CONFIG_DEFAULTS,Setting)
-    except ModuleNotFoundError:
-        logger.warn("Failed to updated cron settings, app not installed")
-
-    # Update Corepoint Export Module
-    try:
-        from corepoint_export.helpers import settings_fields as corepoint_fields
-        configuration_fixtures(corepoint_fields.GROUP_CONFIG,corepoint_fields.CONFIG_DEFAULTS,Setting)
-    except ModuleNotFoundError:
-        logger.warn("Failed to updated corepoint_export settings, app not installed")
 
 def reverse_func(apps, schema_editor):
     pass
