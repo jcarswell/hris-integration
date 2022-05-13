200578e5f7cc9abcb7e1d65a481a2dc8f7d07eba
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-05-13 05:29:32 -0600

    Added API root view

--- hirs_integration/extras/api/views.py
+++ hirs_integration/extras/api/views.py
@@ -3,25 +3,36 @@
 
 from hris_integration.api.viewsets import Select2ViewSet
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.routers import APIRootView
 
 from . import serializers
 from extras import models
 
+class ExtrasRootView(APIRootView):
+    def get_view_name(self):
+        return 'Extras'
+
+
+class ExtrasS2RootView(APIRootView):
+    def get_view_name(self):
+        return '_S2_Extras'
+
+
 class S2Countries(Select2ViewSet):
-    queryset = models.Country.objects.filter(is_delete=False)
+    queryset = models.Country.objects.all()
     serializer = serializers.S2CountriesSerializer
 
 
 class CountriesView(ReadOnlyModelViewSet):
-    queryset = models.Country.objects.filter(is_delete=False)
+    queryset = models.Country.objects.all()
     serializer_class = serializers.CountriesSerializer
 
 
 class S2States(Select2ViewSet):
-    queryset = models.State.objects.filter(is_delete=False)
+    queryset = models.State.objects.all()
     serializer = serializers.S2StatesSerializer
 
 
 class StatesView(ReadOnlyModelViewSet):
-    queryset = models.State.objects.filter(is_delete=False)
+    queryset = models.State.objects.all()
     serializer_class = serializers.StatesSerializer
