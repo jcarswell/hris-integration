b1ba5bed94270892c780be9bc127b242b421d82b
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-05-25 07:37:56 -0600

    removed leading slashed

--- hirs_integration/hris_integration/config.example.py
+++ hirs_integration/hris_integration/config.example.py
@@ -19,19 +19,17 @@ ALLOWED_HOSTS = [
     #'www.example.com'
 ]
 
-# The datbase connection information. Supported backends are:
+# The database connection information. Supported backends are:
 #  - mssql
 #  - django.db.backends.postgresql
 #  - django.db.backends.mysql
 DATABASE = {
-    'ENGINE': 'mssql',
-    'HOST': '',
-    'NAME': '',
+    "ENGINE": "mssql",
+    "HOST": "",
+    "NAME": "",
     #'USERNAME': '',
     #'PASSWORD': '',
-    'OPTIONS': {
-        'driver': 'SQL Server Native Client 11.0'
-    }
+    "OPTIONS": {"driver": "SQL Server Native Client 11.0"},
 }
 
 #
@@ -41,29 +39,29 @@ DATABASE = {
 #
 
 # The system time zone. (DEFAULT: UTC)
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 # The directory where static files will be stored. (DEFAULT: static)
 # If the path is not absolute, the path will be localized to the app directory.
-STATIC_ROOT = 'static'
+STATIC_ROOT = "static"
 
-# The URL that will be used to access the static files. Must end with a '/' (DEFAULT: /static/)
-STATIC_ROOT_URL = '/static/'
+# The URL that will be used to access the static files. Must end with a '/' (DEFAULT: static/)
+STATIC_ROOT_URL = "static/"
 
 # The directory where media files will be stored. (DEFAULT: media)
 # If the path is not absolute, the path will be localized to the app directory.
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = "media"
 
-# The URL that will be used to access the media files. Must end with a '/' (DEFAULT: /media/)
-MEDIA_URL = '/media/'
+# The URL that will be used to access the media files. Must end with a '/' (DEFAULT: media/)
+MEDIA_URL = "media/"
 
 # Additional apps to be installed. (DEFAULT: [])
 # `corepoint_export` is and optional module representing employee exports that are not to AD.
-ADDITIONAL_APPS = ['corepoint_export']
+ADDITIONAL_APPS = ["corepoint_export"]
 
 # The length of password to generate for new employees. (DEFAULT: 12)
 PASSWORD_LENGTH = 12
 
 # The characters that will be used in employee password generation.
 # (DEFAULT: abcdefABCDEF1234567890!@#$%^&*())
-PASSWORD_CHARS = 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789'
+PASSWORD_CHARS = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789"
