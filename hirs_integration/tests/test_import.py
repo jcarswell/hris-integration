4b71497827283b535dd1d74e6fb49bf7f28d8a11
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-05-28 09:26:59 -0600

    updated manual import process for new models and added employee pre-import creation to test fuzzy matching

--- hirs_integration/tests/test_import.py
+++ hirs_integration/tests/test_import.py
@@ -1,5 +1,5 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 import json
 import logging
@@ -10,60 +10,78 @@ from ftp_import.forms import form
 from ftp_import import csv
 from ftp_import.helpers.stats import Stats
 from ftp_import import ObjectCreationError
-from hirs_admin.models import CsvPending
 from pathlib import Path
-from warnings import warn
+from employee.models import Employee
 
-logger = logging.getLogger('test.csv_import')
+logger = logging.getLogger("test.csv_import")
+
+employees = [
+    {
+        "first_name": "Josh",
+        "last_name": "Kelly",
+    },
+    {
+        "first_name": "Mitilda",
+        "last_name": "Jacob",
+    },
+]
+
 
 class manual_import(form):
-    def __init__(self,field_config,**kwargs):
-        super().__init__(field_config,**kwargs)
-        if hasattr(self, 'csv_pending'):
-            self.csv_pending.givenname = kwargs['first_name']
-            self.csv_pending.lastname = kwargs['last_name']
-            self.csv_pending.save()
-        else:
-            warn(f'Stats test will fail as {self.employee_id} is already pending')
+    def save_post(self):
+        from employee.models import Address, Phone
 
-class CsvManual(csv.CsvImport):
+        if self.employee.is_matched:
+            Address.objects.filter(employee=self.employee).delete()
+            Phone.objects.filter(employee=self.employee).delete()
+            self.employee.delete()
+            self.employee.is_matched = False
+            self.employee.save()
+
+
+class PendingImport(csv.CsvImport):
     def add_data(self):
-        for row in range(0,len(self.data)):
+        for row in range(0, len(self.data)):
             try:
-                manual_import(self.fields,**self.data[row])
+                manual_import(self.fields, **self.data[row])
             except ValueError as e:
-                logger.error("Failed to save Employee refere to previous logs for more details")
+                logger.error(
+                    "Failed to save Employee refer to previous logs for more details"
+                )
                 Stats.errors.append(f"Line: {row} - Error: {e}")
             except ObjectCreationError as e:
-                logger.error("Caught exception while creating employee, failed to create reference object. "
-                             "Refer to above logs")
+                logger.error(
+                    "Caught exception while creating employee, failed to create reference object. "
+                    "Refer to above logs"
+                )
                 Stats.errors.append(f"Line: {row} - Error: {e}")
+
 
 class TestCSVImport(unittest.TestCase):
     def setUp(self):
         Stats.time_start = time.time()
 
     def test_base_import(self):
-        FILE = 'employee_data.csv'
-        path = str(Path(__file__).resolve().parent) + '\\' + FILE
+        for employee in employees:
+            Employee.objects.get_or_create(**employee)
+        FILE = "employee_data.csv"
+        path = str(Path(__file__).resolve().parent) + "\\" + FILE
         with open(path) as data:
             csv.CsvImport(data)
         Stats.files.append(FILE)
 
     def test_manual_import(self):
-        FILE = 'employee_data2.csv'
-        path = str(Path(__file__).resolve().parent) + '\\' + FILE
+        FILE = "employee_data2.csv"
+        path = str(Path(__file__).resolve().parent) + "\\" + FILE
         with open(path) as data:
-            CsvManual(data)
+            PendingImport(data)
         Stats.files.append(FILE)
 
     def test_stats(self):
         Stats.time_end = time.time
-        self.assertGreater(Stats.rows_processed,26)
-        self.assertEquals(Stats.errors,1)
+        self.assertGreater(Stats.rows_processed, 26)
+        self.assertEquals(Stats.errors, 1)
         self.assertTrue(4 <= len(Stats.pending_users) <= 5)
 
     def tearDown(self) -> None:
         logger.info(Stats())
-        
-        
