193f927a992f4df695faadd334c93d99e7fa47a7
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-05-28 06:22:16 -0600

    Added related field lookup to fix saving issue

--- hirs_integration/user_applications/api/serializers.py
+++ hirs_integration/user_applications/api/serializers.py
@@ -1,35 +1,42 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from hris_integration.api.serializers import Select2Serializer,Select2Meta
-from rest_framework.serializers import ModelSerializer
+from hris_integration.api.serializers import Select2Serializer, Select2Meta
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 
 from user_applications import models
+
 
 class s2SoftwareSerializer(Select2Serializer):
     class Meta(Select2Meta):
         model = models.Software
-        field_id = 'id'
-        field_text = ['name']
+        field_id = "id"
+        field_text = ["name"]
 
 
 class SoftwareSerializer(ModelSerializer):
     class Meta:
         model = models.Software
-        fields = '__all__'
-        read_only_fields = ['id']
+        fields = "__all__"
+        read_only_fields = ["id"]
 
 
 class S2AccountSerializer(Select2Serializer):
+    employee = PrimaryKeyRelatedField(queryset=models.Employee.objects.all())
+    software = PrimaryKeyRelatedField(queryset=models.Software.objects.all())
+
     class Meta(Select2Meta):
         model = models.Account
-        field_id = 'id'
-        field_text = ['software']
+        field_id = "id"
+        field_text = ["software"]
 
 
 class AccountSerializer(ModelSerializer):
+    employee = PrimaryKeyRelatedField(queryset=models.Employee.objects.all())
+    software = PrimaryKeyRelatedField(queryset=models.Software.objects.all())
+
     class Meta:
         model = models.Account
-        fields = '__all__'
-        read_only_fields = ['id']
+        fields = "__all__"
+        read_only_fields = ["id"]
         depth = 1
