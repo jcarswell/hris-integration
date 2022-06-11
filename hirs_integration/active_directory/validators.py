7e49c02eaa5dcf2d510f495a7054cd4faca6e320
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-06-11 08:00:43 -0600

    Added None option to values

--- hirs_integration/active_directory/validators.py
+++ hirs_integration/active_directory/validators.py
@@ -1,39 +1,42 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 import re
 import pyodbc
 import logging
 
-from hris_integration.validators import import_validator,ValidationError
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _t
 from django.db import utils
-from .helpers.config import CONFIG_CAT,BASE_DN,get_config
+from .helpers.config import CONFIG_CAT, BASE_DN, get_config
 from .query import Query
 from pyad import ADUser
 
-logger = logging.getLogger('active_directory.validators')
+logger = logging.getLogger("active_directory.validators")
 
-__all__ = ('DnValidator','ad_groups','ad_ous')
+__all__ = ("DnValidator", "ad_groups", "ad_ous")
 
-INIT_ERROR = [('Not Loaded','System not initalized')]
+INIT_ERROR = [("Not Loaded", "System not initialized")]
+
 
 class DnValidator(RegexValidator):
-    regex = r'^((CN=([^,]*)),)?((((?:CN|OU)=[^,]+,?)+),)?((DC=[^,]+,?)+)$',
-    message=_t("Not a valid DN string")
-    flags=re.IGNORECASE
+    regex = (r"^((CN=([^,]*)),)?((((?:CN|OU)=[^,]+,?)+),)?((DC=[^,]+,?)+)$",)
+    message = _t("Not a valid DN string")
+    flags = re.IGNORECASE
+
 
 def ad_groups(dn=None):
     ad_query = Query()
-    
+
     try:
-        base_dn = dn or get_config(CONFIG_CAT,BASE_DN)
-        ad_query.execute_query(attributes=["name","distinguishedName"], 
-                            where_clause="objectClass = 'group'",
-                            base_dn=base_dn)
-        
-        return ad_query.get_all_results_tuple()
+        base_dn = dn or get_config(CONFIG_CAT, BASE_DN)
+        ad_query.execute_query(
+            attributes=["name", "distinguishedName"],
+            where_clause="objectClass = 'group'",
+            base_dn=base_dn,
+        )
+
+        return [(None, "")] + ad_query.get_all_results_tuple()
     except pyodbc.ProgrammingError:
         logger.warning("Caught pyodbc.ProgrammingError")
         return INIT_ERROR
@@ -44,19 +47,22 @@ def ad_groups(dn=None):
         logger.info("Caught Attribute Error, this is likely due to pre-init")
         return INIT_ERROR
 
+
 def ad_ous(dn=None):
     ad_query = Query()
-    output = []
+    output = [(None, "")]
     try:
-        base_dn = dn or get_config(CONFIG_CAT,BASE_DN)
-        ad_query.execute_query(attributes=["distinguishedName"], 
-                            where_clause="objectCategory = 'organizationalUnit'",
-                            base_dn=base_dn)
-        
+        base_dn = dn or get_config(CONFIG_CAT, BASE_DN)
+        ad_query.execute_query(
+            attributes=["distinguishedName"],
+            where_clause="objectCategory = 'organizationalUnit'",
+            base_dn=base_dn,
+        )
+
         res = ad_query.get_all_results()
         for r in res:
-            output.append((r["distinguishedName"],r["distinguishedName"]))
-        
+            output.append((r["distinguishedName"], r["distinguishedName"]))
+
         return output
     except pyodbc.ProgrammingError:
         logger.warning("Caught pyodbc.ProgrammingError")
@@ -68,19 +74,22 @@ def ad_ous(dn=None):
         logger.info("Caught Attribute Error, this is likely due to pre-init")
         return INIT_ERROR
 
+
 def ad_user_dn(dn=None):
     ad_query = Query()
-    users = []
+    users = [(None, "")]
     try:
-        base_dn = dn or get_config(CONFIG_CAT,BASE_DN)
-        ad_query.execute_query(attributes=["distinguishedName","objectClass"], 
-                            where_clause="objectClass = 'user'",
-                            base_dn=base_dn)
-        
+        base_dn = dn or get_config(CONFIG_CAT, BASE_DN)
+        ad_query.execute_query(
+            attributes=["distinguishedName", "objectClass"],
+            where_clause="objectClass = 'user'",
+            base_dn=base_dn,
+        )
+
         for user in ad_query.get_results():
             if user["objectClass"] != "computer":
                 u = ADUser.from_dn(user["distinguishedName"])
-                users.append((u.name,user["distinguishedName"]))
+                users.append((u.name, user["distinguishedName"]))
 
         return users
     except pyodbc.ProgrammingError:
@@ -93,19 +102,22 @@ def ad_user_dn(dn=None):
         logger.info("Caught Attribute Error, this is likely due to pre-init")
         return INIT_ERROR
 
+
 def ad_user_guid(dn=None):
     ad_query = dn or Query()
-    users = []
+    users = [(None, "")]
     try:
-        base_dn = get_config(CONFIG_CAT,BASE_DN)
-        ad_query.execute_query(attributes=["distinguishedName","objectClass"], 
-                            where_clause="objectClass = 'user'",
-                            base_dn=base_dn)
-        
+        base_dn = get_config(CONFIG_CAT, BASE_DN)
+        ad_query.execute_query(
+            attributes=["distinguishedName", "objectClass"],
+            where_clause="objectClass = 'user'",
+            base_dn=base_dn,
+        )
+
         for user in ad_query.get_results():
             if user["objectClass"] != "computer":
                 u = ADUser.from_dn(user["distinguishedName"])
-                users.append((u.name,u.guid_str))
+                users.append((u.name, u.guid_str))
 
         return users
 
