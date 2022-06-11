832d7f734e512ad2ffe0022ca5762836a55c8098
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-06-11 06:43:54 -0600

    updated to match new model field names. And resolved the empty group being not valid warning

--- hirs_integration/organization/group_manager.py
+++ hirs_integration/organization/group_manager.py
@@ -1,5 +1,5 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 import logging
 
@@ -7,80 +7,100 @@ from pyad import ADGroup
 from organization.models import GroupMapping
 from common import validators
 
-logger = logging.getLogger('ad_export.GroupManager')
+logger = logging.getLogger("ad_export.GroupManager")
+
 
 class GroupManager:
-    def __init__(self,job,bu,location):
+    def __init__(self, job, bu, location):
         self.add_groups = []
         self.remove_groups = []
         all_groups = GroupMapping.objects.all()
-        
+
         for group in all_groups:
             if group.all:
                 self._add(group.dn)
 
-        self.get_jobs(all_groups,job)
-        self.get_business_units(all_groups,bu)
-        self.get_locations(all_groups,location)
+        self.get_jobs(all_groups, job)
+        self.get_business_units(all_groups, bu)
+        self.get_locations(all_groups, location)
         self.parse_config_groups()
 
-    def get_jobs(self,groups,job):
+    def get_jobs(self, groups, job):
         for group in groups:
-            if self.__iter_group(group.jobs,job) and group.jobs_not == False:
+            if self.__iter_group(group.jobs, job) and group.jobs_not == False:
                 self._add(group.dn)
-            elif self.__iter_group(group.jobs,job) and group.jobs_not == True:
+            elif self.__iter_group(group.jobs, job) and group.jobs_not == True:
                 self._remove(group.dn)
-            elif not self.__iter_group(group.jobs,job) and group.jobs_not == True:
+            elif not self.__iter_group(group.jobs, job) and group.jobs_not == True:
                 self._add(group.dn)
 
-    def get_business_units(self,groups,business_unit):
+    def get_business_units(self, groups, business_unit):
         for group in groups:
-            if self.__iter_group(group.bu,business_unit) and group.bu_not == False:
+            if (
+                self.__iter_group(group.business_unit, business_unit)
+                and group.business_unit_not == False
+            ):
                 self._add(group.dn)
-            elif self.__iter_group(group.bu,business_unit) and group.bu_not == True:
+            elif (
+                self.__iter_group(group.business_unit, business_unit)
+                and group.business_unit_not == True
+            ):
                 self._remove(group.dn)
-            elif not self.__iter_group(group.bu,business_unit) and group.bu_not == True:
+            elif (
+                not self.__iter_group(group.business_unit, business_unit)
+                and group.business_unit_not == True
+            ):
                 self._add(group.dn)
 
-    def get_locations(self,groups,location):
+    def get_locations(self, groups, location):
         for group in groups:
-            if self.__iter_group(group.loc,location) and group.loc_not == False:
+            if (
+                self.__iter_group(group.location, location)
+                and group.location_not == False
+            ):
                 self._add(group.dn)
-            elif self.__iter_group(group.loc,location) and group.loc_not == True:
+            elif (
+                self.__iter_group(group.location, location)
+                and group.location_not == True
+            ):
                 self._remove(group.dn)
-            elif not self.__iter_group(group.loc,location) and group.loc_not == True:
+            elif (
+                not self.__iter_group(group.location, location)
+                and group.location_not == True
+            ):
                 self._add(group.dn)
 
-    def _add(self,group):
+    def _add(self, group):
         if group in self.remove_groups:
             logger.debug(f"Skipping _add for '{group}' as it exists in remove groups")
         elif group not in self.add_groups:
             logger.debug(f"Added group to add_groups: {group}")
             self.add_groups.append(group)
 
-    def _remove(self,group):
+    def _remove(self, group):
         if group in self.add_groups:
             logger.debug(f"Removing '{group}' from add_groups")
             self.add_groups.pop(self.add_groups.index(group))
-        
+
         if group not in self.remove_groups:
             logger.debug(f"Adding remove group: {group}")
             self.remove_groups.append(group)
 
     def parse_config_groups(self):
         """Parse config groups by dn or cn"""
-        from organization.helpers.config import Config,GROUP_CAT,GROUPS_LEAVE_GROUP
-        config = Config()
-        self.groups_leave = self.parse_group(config(GROUP_CAT,GROUPS_LEAVE_GROUP))
+        from organization.helpers.config import Config, GROUPS_CAT, GROUPS_LEAVE_GROUP
 
-    def parse_group(self,groups:str) -> list:
+        config = Config()
+        self.groups_leave = self.parse_group(config(GROUPS_CAT, GROUPS_LEAVE_GROUP))
+
+    def parse_group(self, groups: str) -> list:
         output = []
         dn = []
         logger.debug(f"parsing the following groups: {groups}")
-        for group in groups.strip('\'"').split(','):
-            if len(group.split('=')) == 1:
+        for group in groups.strip("'\"").split(","):
+            if group and len(group.split("=")) == 1:
                 if dn != []:
-                    output.append(','.join(dn))
+                    output.append(",".join(dn))
                     dn = []
                 try:
                     output.append(ADGroup.from_cn(group).dn)
@@ -89,20 +109,20 @@ class GroupManager:
                     logger.debug(f"Caught exception while retrieving group: {e}")
 
             else:
-                if group[0:2].lower() == "cn=":
+                if group[:3].lower() == "cn=":
                     if dn != []:
-                        output.append(','.join(dn))
+                        output.append(",".join(dn))
                         dn = []
                     dn.append(group)
-                else:
+                elif group:
                     dn.append(group)
-        
-        #Ensure that we're not leaving a DN out of the output
-        if dn != []:
-            output.append(','.join(dn))
 
-        #Check that we are only returning valid DN's
-        for x in range(0,len(output)):
+        # Ensure that we're not leaving a DN out of the output
+        if dn != []:
+            output.append(",".join(dn))
+
+        # Check that we are only returning valid DN's
+        for x in range(0, len(output)):
             try:
                 validators.DnValidator(output[x])
             except validators.ValidationError:
@@ -110,9 +130,9 @@ class GroupManager:
                 output.pop(x)
 
         return output
-    
+
     @staticmethod
-    def __iter_group(set,value):
+    def __iter_group(set, value):
         for item in set.all():
             if item == value:
                 return True
