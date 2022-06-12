4d3632603b4dd2ebdd29914a43a89e7b0cae9345
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-06-12 11:54:03 -0600

    added license

--- hirs_integration/hris_integration/models/manager.py
+++ hirs_integration/hris_integration/models/manager.py
@@ -1,4 +1,8 @@
+# Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 from django.db.models import Manager
+
 
 class InactiveManager(Manager):
     """Model manager that provides extra functionality for the InactiveMixin"""
