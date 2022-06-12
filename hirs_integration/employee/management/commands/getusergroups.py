a6292427b8ccdab671fc9a8b329f72a995392d24
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-06-12 08:02:17 -0600

    Updated to use base employee manager

--- hirs_integration/employee/management/commands/getusergroups.py
+++ hirs_integration/employee/management/commands/getusergroups.py
@@ -1,7 +1,8 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     help = "Run the AD Export job"
@@ -10,56 +11,63 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '--id',
-            nargs='+',
-            type=int,
-            help='get groups for specified employee IDs'
+            "--id", nargs="+", type=int, help="get groups for specified employee IDs"
         )
         parser.add_argument(
-            '--username',
-            nargs='+',
-            type=str,
-            help='get groups for specified usernames'
+            "--username", nargs="+", type=str, help="get groups for specified usernames"
         )
         parser.add_argument(
-            '--emailalias',
-            nargs='+',
+            "--emailalias",
+            nargs="+",
             type=str,
-            help='get groups for specified email aliases'
+            help="get groups for specified email aliases",
         )
 
     def handle(self, *args, **kwargs):
-        for e in self.get_employees(kwargs['id'],kwargs['username'],kwargs['emailalias']):
+        for e in self.get_employees(
+            kwargs["id"], kwargs["username"], kwargs["emailalias"]
+        ):
             self.stdout.write(self.style.SUCCESS(str(e)))
-            self.stdout.write(self.style.NOTICE('Member of Groups'))
+            self.stdout.write(self.style.NOTICE("Member of Groups"))
             self.stdout.writelines(e.groups_add())
-            self.stdout.write(self.style.WARNING('Removed from Groups'))
+            self.stdout.write(self.style.WARNING("Removed from Groups"))
             self.stdout.writelines(e.groups_remove())
 
-    def get_employees(self,ids=None,users=None,aliases=None):
-        from ad_export.helpers import config
+    def get_employees(self, ids=None, users=None, aliases=None):
+        from employee.data_structures import EmployeeManager
         from employee.models import Employee
+
         employees = []
 
         if ids:
             for id in ids:
                 try:
-                    employees.append(config.EmployeeManager(Employee.objects.get(employee_id=id)))
+                    employees.append(
+                        EmployeeManager(Employee.objects.get(employee_id=id))
+                    )
                 except Employee.DoesNotExist:
                     self.stdout.write(self.style.ERROR(f"No employee with id {id}"))
 
         if users:
             for user in users:
                 try:
-                    employees.append(config.EmployeeManager(Employee.objects.get(username=user)))
+                    employees.append(
+                        EmployeeManager(Employee.objects.get(username=user))
+                    )
                 except Employee.DoesNotExist:
-                    self.stdout.write(self.style.ERROR(f"No employee with username {user}"))
+                    self.stdout.write(
+                        self.style.ERROR(f"No employee with username {user}")
+                    )
 
         if aliases:
             for alias in aliases:
                 try:
-                    employees.append(config.EmployeeManager(Employee.objects.get(email_alias=alias)))
+                    employees.append(
+                        EmployeeManager(Employee.objects.get(email_alias=alias))
+                    )
                 except Employee.DoesNotExist:
-                    self.stderr.write(self.style.ERROR(f"No employee with alias: {alias}"))
+                    self.stderr.write(
+                        self.style.ERROR(f"No employee with alias: {alias}")
+                    )
 
         return employees
