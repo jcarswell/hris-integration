8b39a3ac5ace595f29c11d62a5e0fcb3bb6992a9
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-06-12 08:03:30 -0600

    Formatted Black

--- hirs_integration/user_applications/models.py
+++ hirs_integration/user_applications/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 from django.core.exceptions import ValidationError
-from django.db.models.signals import pre_save,post_save
+from django.db.models.signals import pre_save
 from employee.models import Employee
 from hris_integration.models import ChangeLogMixin
 
 
 class Software(ChangeLogMixin):
-    """A list of software or applications that one may wish to track for an employee. 
+    """
+    A list of software or applications that one may wish to track for an employee.
     Each software instance has the ability to limit the total number of licenses
-    if that is desired. When licensing is enabled the associated employees are 
+    if that is desired. When licensing is enabled the associated employees are
     tracked for the specific software instance.
 
     The employees field is updated when creating a new tracking record.
     """
 
     class Meta:
-        db_table = 'software'
+        db_table = "software"
 
     id = models.AutoField(primary_key=True)
     #: str: The name of the software.
@@ -43,7 +44,7 @@ class Account(ChangeLogMixin):
     """
 
     class Meta:
-        db_table = 'software-account'
+        db_table = "software-account"
 
     id = models.AutoField(primary_key=True)
     #: Employee: The employee linked to this tracking instance.
@@ -52,7 +53,7 @@ class Account(ChangeLogMixin):
     software = models.ForeignKey(Software, on_delete=models.CASCADE)
     #: str: Notes related to this specific tracking instance.
     notes = models.TextField(blank=True)
-    #: date: The date that the license/allocation expires.    
+    #: date: The date that the license/allocation expires.
     expire_date = models.DateField(blank=True, null=True)
 
     def __str__(self) -> str:
@@ -60,27 +61,34 @@ class Account(ChangeLogMixin):
 
     @classmethod
     def pre_save(cls, sender, instance, raw, using, update_fields, **kwargs):
-        if hasattr(instance, 'software') and hasattr(instance, 'employee'):
+        if hasattr(instance, "software") and hasattr(instance, "employee"):
             prev_instance = None
-            if hasattr(instance,'id') and instance.id:
+            if hasattr(instance, "id") and instance.id:
                 try:
                     prev_instance = Account.objects.get(id=instance.id)
                 except Account.DoesNotExist:
                     pass
 
             if prev_instance:
-                if (prev_instance.software 
-                    and prev_instance.software.id != instance.software.id):
+                if (
+                    prev_instance.software
+                    and prev_instance.software.id != instance.software.id
+                ):
                     raise ValidationError("Software instance is not mutable")
-                if (prev_instance.employee and 
-                    prev_instance.employee.id != instance.employee.id):
+                if (
+                    prev_instance.employee
+                    and prev_instance.employee.id != instance.employee.id
+                ):
                     raise ValidationError("Employee is not mutable")
                 if prev_instance.software is None or prev_instance.employee is None:
-                    if (len(Account.objects.filter(software=instance.software)) 
-                        >= instance.software.max_users 
-                        and (instance.software.licensed or instance.software.max_users)):
+                    if len(
+                        Account.objects.filter(software=instance.software)
+                    ) >= instance.software.max_users and (
+                        instance.software.licensed or instance.software.max_users
+                    ):
                         raise ValidationError("License limit exceeded")
                     instance.software.employees.add(instance.employee)
                     instance.software.save()
+
 
 pre_save.connect(Account.pre_save, sender=Account)
