9aa5ec22fbeb2d94b602085b23b5618abd5a7074
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-06-12 08:48:45 -0600

    Added doc string to get_by_path

--- hirs_integration/settings/models.py
+++ hirs_integration/settings/models.py
@@ -1,5 +1,5 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 import logging
 import json
@@ -13,7 +13,7 @@ from django.utils.translation import gettext_lazy as _t
 from string import ascii_letters, digits, capwords
 from warnings import warn
 
-logger = logging.getLogger('settings.Models')
+logger = logging.getLogger("settings.Models")
 
 #######################
 #####            ######
@@ -21,8 +21,25 @@ logger = logging.getLogger('settings.Models')
 #####            ######
 #######################
 
+
 class SettingsManager(models.Manager):
-    def get_by_path(self, group:str, category:str =None, item:str =None) -> QuerySet:
+    def get_by_path(
+        self, group: str, category: str = None, item: str = None
+    ) -> QuerySet:
+        """
+        Get a setting by setting path (group/category/item), this path category and item
+        are optional allowing for all items to be retrieved from a group or category.
+
+        :param group: The root group to search
+        :type group: str
+        :param category: The optional category to search
+        :type category: str, optional
+        :param item: The optional item to retrieve
+        :type item: str, optional
+        :return: A QuerySet based on the path provided
+        :rtype: QuerySet
+        """
+
         path = group + Setting.FIELD_SEP
 
         if category:
@@ -34,28 +51,30 @@ class SettingsManager(models.Manager):
 
         return self.filter(setting__startswith=path)
 
+
 class Setting(models.Model):
     """User Configurable Application Settings"""
-    class Meta:
-        db_table = 'setting'
 
-    #: CONST str: The field separator for the setting path 
-    FIELD_SEP = '/'
+    class Meta:
+        db_table = "setting"
+
+    #: CONST str: The field separator for the setting path
+    FIELD_SEP = "/"
     #: CONST str: The base field type for a setting
-    DEFAULT_FIELD = 'CharField'
+    DEFAULT_FIELD = "CharField"
     #: CONST dict: The base field properties for a setting
     __BASE_PROPERTIES__ = {
-        'type': DEFAULT_FIELD,
-        'required': False,
-        'disabled': False,
+        "type": DEFAULT_FIELD,
+        "required": False,
+        "disabled": False,
     }
 
     #: str: The setting path
-    setting = models.CharField(max_length=768,unique=True)
+    setting = models.CharField(max_length=768, unique=True)
     #: str: The setting value
-    _value = models.TextField(null=True,blank=True)
+    _value = models.TextField(null=True, blank=True)
     #: str: The field properties for the setting allowing for correct type conversion and rendering
-    _field_properties = models.TextField(null=True,blank=True)
+    _field_properties = models.TextField(null=True, blank=True)
     #: bool: Whether the value is encrypted in the database or stored in plaintext
     hidden = models.BooleanField(default=False)
 
@@ -69,13 +88,13 @@ class Setting(models.Model):
             return FieldEncryption().decrypt(self._value)
         else:
             return self._value
-      
-    @value.setter  
-    def value(self, value:str) -> None:
+
+    @value.setter
+    def value(self, value: str) -> None:
         """Sets the value of the setting, encrypting if the hidden property is set"""
         if value == None:
-            value = ''
-        if not isinstance(value,str):
+            value = ""
+        if not isinstance(value, str):
             raise ValueError(f"Expected a str got {value.__class__.__name__}")
         if self.hidden:
             self._value = FieldEncryption().encrypt(value)
@@ -84,7 +103,7 @@ class Setting(models.Model):
 
     def __str__(self) -> str:
         """The string representation of the setting (returns the raw value)"""
-        
+
         return f"{self.setting} - {self._value}"
 
     def __init__(self, *args, **kwargs) -> None:
@@ -107,29 +126,40 @@ class Setting(models.Model):
 
     @classmethod
     def pre_save(cls, sender, instance, raw, using, update_fields, **kwargs):
-        """Ensure that any invalid characters in the setting path are removed and that the 
+        """Ensure that any invalid characters in the setting path are removed and that the
         field properties are valid are converted to a JSON string"""
-        
-        field_types = ('CharField','ChoiceField','DateField','BooleanField',
-                       'DecimalField','FloatField',
-                       'DateTimeField','RegexField','IntegerField')
+
+        field_types = (
+            "CharField",
+            "ChoiceField",
+            "DateField",
+            "BooleanField",
+            "DecimalField",
+            "FloatField",
+            "DateTimeField",
+            "RegexField",
+            "IntegerField",
+        )
 
         for char in instance.setting:
-            if char not in ascii_letters + digits + instance.FIELD_SEP + '_-':
-                instance.setting.replace(char,'_')
- 
-        if len(instance.setting.split(instance.FIELD_SEP)) != 3:
-            raise ValueError("setting does not contain proper format, should be group/category/item")
+            if char not in ascii_letters + digits + instance.FIELD_SEP + "_-":
+                instance.setting.replace(char, "_")
 
-        if instance.field_properties['type'] not in field_types:
+        if len(instance.setting.split(instance.FIELD_SEP)) != 3:
+            raise ValueError(
+                "setting does not contain proper format, should be group/category/item"
+            )
+
+        if instance.field_properties["type"] not in field_types:
             raise ValueError(f"Field type must be one of {field_types}")
-        
-        if (not instance._field_properties or 
-                (json.loads(instance._field_properties) != instance.field_properties)):
+
+        if not instance._field_properties or (
+            json.loads(instance._field_properties) != instance.field_properties
+        ):
             instance._field_properties = json.dumps(instance.field_properties)
 
     @staticmethod
-    def _as_text(text:str) -> str:
+    def _as_text(text: str) -> str:
         """Converts a string to a title case string that is also translatable"""
 
         if _t(text) == text:
@@ -152,7 +182,10 @@ class Setting(models.Model):
 
     @property
     def catagory(self) -> str:
-        warn("Setting to catagory is deprecated, use category instead", DeprecationWarning)
+        warn(
+            "Setting to catagory is deprecated, use category instead",
+            DeprecationWarning,
+        )
         return self.category
 
     @property
@@ -175,7 +208,10 @@ class Setting(models.Model):
 
     @property
     def catagory_text(self) -> str:
-        warn("Setting to catagory_text is deprecated, use category_text instead", DeprecationWarning)
+        warn(
+            "Setting to catagory_text is deprecated, use category_text instead",
+            DeprecationWarning,
+        )
         return self.category_text
 
     @property
@@ -183,13 +219,16 @@ class Setting(models.Model):
         """The title case item name of the setting"""
 
         return self._as_text(self.setting.split(self.FIELD_SEP)[2])
+
+
 pre_save.connect(Setting.pre_save, sender=Setting)
 
 
 class WordList(models.Model):
     """A list of shorthand words and there expanded form for the word expansion feature"""
+
     class Meta:
-        db_table = 'word_list'
+        db_table = "word_list"
 
     #: str: The word to be expanded
     src = models.CharField(max_length=256)
