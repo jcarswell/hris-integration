527db0bff525e1c553fda2cd646dbcd8f8ab437e
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-06-16 06:21:59 -0600

    Fixed validation and added preform actions

--- hirs_integration/user_applications/api/serializers.py
+++ hirs_integration/user_applications/api/serializers.py
@@ -1,10 +1,19 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+import logging
+
 from hris_integration.api.serializers import Select2Serializer, Select2Meta
-from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from rest_framework.serializers import (
+    ModelSerializer,
+    PrimaryKeyRelatedField,
+    ValidationError,
+)
+from rest_framework.validators import UniqueTogetherValidator, UniqueValidator
 
 from user_applications import models
+
+logger = logging.getLogger("user_applications.api.serializers")
 
 
 class s2SoftwareSerializer(Select2Serializer):
@@ -32,11 +41,38 @@ class S2AccountSerializer(Select2Serializer):
 
 
 class AccountSerializer(ModelSerializer):
-    employee = PrimaryKeyRelatedField(queryset=models.Employee.objects.all())
-    software = PrimaryKeyRelatedField(queryset=models.Software.objects.all())
+    employee = PrimaryKeyRelatedField(
+        queryset=models.Employee.objects.all(), required=True
+    )
+    software = PrimaryKeyRelatedField(
+        queryset=models.Software.objects.all(), required=True
+    )
 
     class Meta:
         model = models.Account
         fields = "__all__"
         read_only_fields = ["id"]
         depth = 1
+        validators = [
+            UniqueTogetherValidator(
+                queryset=models.Account.objects.all(),
+                fields=["employee", "software"],
+                message="Account already exists for this employee.",
+            )
+        ]
+
+    def validate_software(self, value):
+        software = None
+        if isinstance(value, (str, int)):
+            software = models.Software.objects.get(id=int(value))
+        elif isinstance(value, models.Software):
+            software = value
+
+        if (
+            software.max_users
+            and len(models.Account.objects.filter(software=software))
+            >= software.max_users
+        ):
+            raise ValidationError("Software already has the maximum number of users.")
+
+        return software
--- hirs_integration/user_applications/api/views.py
+++ hirs_integration/user_applications/api/views.py
@@ -1,46 +1,111 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import logging
 
 from hris_integration.api.viewsets import Select2ViewSet
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import DjangoModelPermissions, IsAuthenticated
 from rest_framework.routers import APIRootView
+from rest_framework.serializers import ValidationError
 
 from . import serializers
 from user_applications import models
 
+logger = logging.getLogger("user_applications.api.views")
+
+
 class UserApplicationsRootView(APIRootView):
     def get_view_name(self):
-        return 'User Applications'
+        return "User Applications"
 
 
 class UserApplicationsS2RootView(APIRootView):
     def get_view_name(self):
-        return 'User Applications Select2'
+        return "User Applications Select2"
 
 
 class S2Software(Select2ViewSet):
     queryset = models.Software.objects.all()
     serializer_class = serializers.s2SoftwareSerializer
-    filterset_fields = ['name','licensed']
+    filterset_fields = ["name", "licensed"]
+
 
 class Software(ModelViewSet):
     queryset = models.Software.objects.all()
     serializer_class = serializers.SoftwareSerializer
-    read_only_fields = ['id']
-    filterset_fields = ['name','licensed']
-    permission_classes = [DjangoModelPermissions|IsAuthenticated]
+    read_only_fields = ["id"]
+    filterset_fields = ["name", "licensed"]
+    permission_classes = [DjangoModelPermissions | IsAuthenticated]
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        for employee in instance.employees.all():
+            models.Account.objects.create(employee=employee, software=instance)
+
+    def perform_update(self, serializer):
+        employees = []
+        if serializer.instance:
+            if (
+                serializer.validated_data
+                and serializer.validated_data.get("licensed", False)
+                != serializer.instance.licensed
+                and serializer.validated_data.get("licensed", False) == True
+                or serializer.validated_data.get("max_users", 0)
+                != serializer.instance.max_users
+                and serializer.validated_date.get("max_users", 0) > 0
+            ):
+                for account in models.Account.objects.filter(
+                    software=serializer.instance
+                ):
+                    employees.append(account.employee)
+            else:
+                employees = None
+            if serializer.validated_data.get(
+                "max_users", serializer.instance.max_users
+            ) > len(employees):
+                raise ValidationError(
+                    f"There are currently {len(employees)} employees using this"
+                    " software. Which is more than the maximum allowed users."
+                )
+            else:
+                instance = serializer.save()
+                if employees:
+                    for employee in employees:
+                        instance.employees.add(employee)
+                    instance.save()
+                elif employees is None:
+                    for employee in instance.employees.all():
+                        instance.employees.remove(employee)
+                    instance.save()
+        else:
+            raise ValidationError(
+                "In perform update Software and serializer instance is None"
+            )
 
 
 class S2Account(Select2ViewSet):
     queryset = models.Account.objects.all()
     serializer_class = serializers.S2AccountSerializer
-    filterset_fields = ['software','employee','expire_date']
+    filterset_fields = ["software", "employee", "expire_date"]
 
 
 class Account(ModelViewSet):
     queryset = models.Account.objects.all()
     serializer_class = serializers.AccountSerializer
-    read_only_fields = ['id']
-    permission_classes = [DjangoModelPermissions|IsAuthenticated]
-    filterset_fields = ['software','employee','expire_date']
+    read_only_fields = ["id"]
+    permission_classes = [DjangoModelPermissions | IsAuthenticated]
+    filterset_fields = ["software", "employee", "expire_date"]
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        if instance.employee and instance.software:
+            if instance.software.licensed or instance.software.max_users:
+                instance.software.employees.add(instance.employee)
+
+    def perform_destroy(self, instance):
+        try:
+            instance.software.employees.remove(instance.employee)
+        except Exception:
+            logger.debug("Could not remove employee from software.")
+        instance.delete()
