79160f217fa1e6601cf3de568c00c1dbef18b231
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-06-19 08:48:53 -0600

    Updated exception import

--- hirs_integration/active_directory/query.py
+++ hirs_integration/active_directory/query.py
@@ -2,7 +2,8 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from pyad.adquery import ADQuery
-from pyad.pyadexceptions import noExecutedQuery
+
+from .exceptions import noExecutedQuery
 
 
 class Query(ADQuery):
