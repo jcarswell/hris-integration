cd18c50138f8bb51c9c50e1e1d4b4fefcbfe1b8a
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-06-19 08:40:52 -0600

    Removed duplicated functions

--- hirs_integration/active_directory/query.py
+++ hirs_integration/active_directory/query.py
@@ -1,23 +1,25 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from pyad.adquery import ADQuery
 from pyad.pyadexceptions import noExecutedQuery
+
 
 class Query(ADQuery):
     """
     Extension of the ADQuery class adding return values as tuple
     which is needed for the Django models.
-    
+
     Added functions:
       - get_all_results_tuple: Gets all results from the executed query and returns a list of tuples
       - get_single_result_tuple: Fetches the next result in the result set
     """
+
     __row_counter = 0
-    
+
     def __init__(self, options={}):
         super().__init__(options)
-    
+
     def get_all_results_tuple(self) -> list:
         """
         Get all results and returns them in a list of tuples
@@ -28,12 +30,12 @@ class Query(ADQuery):
         Returns:
             list: list of tuples with the queried values
         """
-        
+
         if not self._ADQuery__queried:
             raise noExecutedQuery
         if not self._ADQuery__rs.EOF:
             self._ADQuery__rs.MoveFirst()
-        
+
         rows = []
         while not self._ADQuery__rs.EOF:
             vals = []
@@ -43,7 +45,7 @@ class Query(ADQuery):
             self._ADQuery__rs.MoveNext()
 
         return rows
-  
+
     def get_single_result_tuple(self) -> tuple:
         """
         Gets the next result in the result set.
@@ -54,25 +56,17 @@ class Query(ADQuery):
         Returns:
             tuple: list of values for the query result
         """
-        
+
         if not self._ADQuery__queried:
             raise noExecutedQuery
         if not self._ADQuery__rs.EOF and self.__row_counter == 0:
             self._ADQuery__rs.MoveFirst()
-        
+
         d = []
         for f in self._ADQuery__rs.Fields:
             d.append(f.Value)
-        
+
         self.__row_counter += 1
         self._ADQuery__rs.MoveNext()
-        
+
         return tuple(d)
-
-    def reset_position(self):
-        self._ADQuery__rs.MoveFirst()
-
-    @property
-    def row_count(self) -> int:
-        return self._ADQuery__rs.RecordCount
-
