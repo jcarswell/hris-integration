1877db6f2c9e013f23efc4fbbbd5a3a493c85e8d
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-06-22 06:37:50 -0600

    formatted black

--- hirs_integration/corepoint_export/helpers/utils.py
+++ hirs_integration/corepoint_export/helpers/utils.py
@@ -1,47 +1,55 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from . import config
 
+
 class CSVSafe:
-    def parse(self,input) -> str:
+    def parse(self, input) -> str:
         try:
-            getattr(self,type(input).__name__)(input)
+            getattr(self, type(input).__name__)(input)
             return self.__str__()
         except AttributeError:
             raise TypeError(f"{type(input)} is not valid for output")
 
-    def str(self,input:str):
+    def str(self, input: str):
         self.value = input
 
-    def int(self,input:int):
+    def int(self, input: int):
         self.value = str(input)
 
-    def bool(self,input:bool):
-        fmt = config.get_config(config.CAT_CONFIG,config.CONFIG_BOOL_EXPORT).split(',',2)
+    def bool(self, input: bool):
+        fmt = config.get_config(config.CAT_CONFIG, config.CONFIG_BOOL_EXPORT).split(
+            ",", 2
+        )
         if input:
             self.value = fmt[0]
         else:
             self.value = fmt[1]
 
-    def bytes(self,input:bytes):
-        self.str(input.decode('utf-8'))
+    def bytes(self, input: bytes):
+        self.str(input.decode("utf-8"))
 
-    def float(self,input:float):
+    def float(self, input: float):
         self.value = str(input)
 
-    def complex(self,input:complex):
+    def complex(self, input: complex):
         self.value = str(input.real)
 
-    def bytearray(self,input:bytearray):
+    def bytearray(self, input: bytearray):
         self.bytes(input)
 
-    def NoneType(self,input):
+    def NoneType(self, input):
         self.value = ""
 
     def __str__(self):
-        if (' ' in self.value or ',' in self.value or '\t' in self.value
-            or '"' in self.value or '\'' in self.value):
+        if (
+            " " in self.value
+            or "," in self.value
+            or "\t" in self.value
+            or '"' in self.value
+            or "'" in self.value
+        ):
             return f'"{self.value}"'
         else:
             return self.value
