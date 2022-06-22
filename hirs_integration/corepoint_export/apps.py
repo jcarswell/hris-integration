b3a3cff379b1791eac325025862f0e68dd046e1a
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-06-22 06:36:19 -0600

    formatted back

--- hirs_integration/corepoint_export/apps.py
+++ hirs_integration/corepoint_export/apps.py
@@ -1,10 +1,11 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _t
 
+
 class CorepointExportConfig(AppConfig):
-    name = 'corepoint_export'
-    verbose_name = _t('Corepoint Export')
-    default_auto_field = 'django.db.models.AutoField'
+    name = "corepoint_export"
+    verbose_name = _t("Corepoint Export")
+    default_auto_field = "django.db.models.AutoField"
