02189c266bea8f3e1664dbedc60d5bb50db2de1b
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-06-25 07:14:29 -0600

    cleanued up imports

--- hirs_integration/organization/migrations/0002_migrate_data.py
+++ hirs_integration/organization/migrations/0002_migrate_data.py
@@ -1,7 +1,6 @@
 # Copyright: (c) 2022, Josh Carswell <josh.carswell@thecarswells.ca>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from typing import Dict
 from django.db import migrations
 import logging
 
